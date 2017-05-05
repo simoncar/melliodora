5935f85601e634be9c7b48e0884cc72bd40e232f
Simoncar <simoncar@gmail.com>
2017-05-05 19:36:50 +0800

    Updates from gitstrap

--- js/setup.js
+++ js/setup.js
@@ -22,6 +22,7 @@ export default class Setup extends Component {
     await Expo.Font.loadAsync({
       Roboto: require('native-base/Fonts/Roboto.ttf'),
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      'Ionicons': require('@expo/vector-icons/fonts/Ionicons.ttf'),
     });
 
     this.setState({ isReady: true });
