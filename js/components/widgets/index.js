5ca03d70a19e56ecc44a71a194ffdf42da2100fb
Simoncar <simoncar@gmail.com>
2017-05-09 09:30:44 +0800

    Cafe top-up button only

--- js/components/widgets/index.js
+++ js/components/widgets/index.js
@@ -11,35 +11,12 @@ import { openDrawer } from '../../actions/drawer';
 import theme from '../../themes/base-theme';
 import styles from './styles';
 
-import * as firebase from 'firebase';
 
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyBLz76NsS1fjNXcaGBUhcp9qA-MFg1Hrg8",
-  authDomain: "calendarapp-b7967.firebaseapp.com",
-  databaseURL: "https://calendarapp-b7967.firebaseio.com",
-  storageBucket: "calendarapp-b7967.appspot.com"
-};
-
-//firebase.initializeApp(firebaseConfig);
-
-function storeHighScore(userId, score) {
-  firebase.database().ref('users/' + userId).set({
-    highscore: score
-  });
-};
 
 const primary = require('../../themes/variable').brandPrimary;
 
 
-function aa(myscore){
-  alert('gata rhe ');
-
-  firebase.database().ref('usersccc').set({
-      highscore: myscore
-    });
-
-};
+var WEBVIEW_REF = 'webview';
 
 class Widgets extends Component {
 
@@ -60,30 +37,27 @@ class Widgets extends Component {
 
             <Right>
               <Button transparent>
-                <Icon active name="add" onPress={() =>aa(4444)} />
+                <Icon active name="ios-restaurant" />
               </Button>
+
             </Right>
           </Header>
-
-
-
-
-
             <WebView
                     source={{uri: 'https://mystamford.edu.sg/login/login.aspx?prelogin=http%3a%2f%2fmystamford.edu.sg%2f&kr=iSAMS:ParentPP'}}
                     javaScriptEnabled={true}
                     domStorageEnabled={true}
-                              startInLoadingState={true}
+                    startInLoadingState={true}
+                    ref={WEBVIEW_REF}
                   />
-
-
         </Image>
       </Container>
     );
   }
+
+
+
+
 }
-
-
 
 
 function bindAction(dispatch) {
