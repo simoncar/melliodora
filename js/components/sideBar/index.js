8d171b7ed723ef3fd16b80493c7b90ba456aa711
mcrosby37 <mcrosby37@yahoo.com>
2017-05-18 15:51:02 +0800

    deleted logout function from side panel

--- js/components/sideBar/index.js
+++ js/components/sideBar/index.js
@@ -29,22 +29,13 @@ class SideBar extends Component {
               <Icon name="ios-calendar-outline" />
               <Text style={styles.linkText} >CALENDAR</Text>
             </ListItem>
-            
+
 
             <View style={styles.logoutContainer}>
               <View style={styles.logoutbtn} foregroundColor={'white'}>
                 <Grid>
-                  <Col>
-                    <TouchableOpacity onPress={() => { Actions.login({ type: ActionConst.RESET  }); this.props.closeDrawer(); }} style={{ alignSelf: 'flex-start' }}>
-                      <Text style={{ fontWeight: 'bold', color: '#fff' }}>LOG OUT</Text>
-                      <Text note style={{ color: '#fff' }} >Matt Crosby</Text>
-                    </TouchableOpacity>
-                  </Col>
-                  <Col>
-                    <TouchableOpacity style={{ alignSelf: 'flex-end' }} onPress={() => { Actions.profile(); this.props.closeDrawer(); }}>
-                      <Thumbnail source={require('../../../images/contacts/sanket.png')} style={styles.profilePic} />
-                    </TouchableOpacity>
-                  </Col>
+
+                  
                 </Grid>
               </View>
             </View>
