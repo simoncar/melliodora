34950c1b2c95f12b9ab3650ecb07a33e3e23fd54
Simoncar <simoncar@gmail.com>
2017-05-31 16:53:03 +0800

    Revert "Reinstate login"
    This reverts commit ea3d11041efaee57cce704c5a7ec73ebfaa541f1.

--- js/components/widgets/index.js
+++ js/components/widgets/index.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {  WebView, Image, View, Platform } from 'react-native';
@@ -44,14 +45,13 @@ class Widgets extends Component {
             </Right>
           </Header>
 
-            <WebView
-                    source={{uri: 'https://mystamford.edu.sg/login/login.aspx?prelogin=http%3a%2f%2fmystamford.edu.sg%2f&kr=iSAMS:ParentPP'}}
-                    javaScriptEnabled={true}
-                    domStorageEnabled={true}
-                    startInLoadingState={true}
-                     injectedJavaScript={injectScript}
-                    ref={WEBVIEW_REF}
-                  />
+          <View>
+
+        <Text  style={styles.weatherTime}> Fetch Recipes </Text>
+{console.log ('hello')}
+    </View>
+
+
         </Image>
       </Container>
     );
