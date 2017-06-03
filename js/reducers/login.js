bb3b3299ce1522df1ba3131b81f2bce1389d0168
Simoncar <simoncar@gmail.com>
2017-06-03 15:38:01 +0800

    Breaking change - reducer

--- js/reducers/login.js
+++ js/reducers/login.js
@@ -5,15 +5,36 @@ import * as types from '../actions/types'
 
 export const recipeCount = createReducer(0, {
   [types.SET_SEARCHED_RECIPES](state,action){
+        console.log('reducer - SET_SEARCHED_RECIPES');
     return action.recipes.length;
   },
 
   [types.ADD_RECIPE](state,action){
+        console.log('reducer - ADD_RECIPE');
     return state+1;
   },
 
   [types.SET_LOGIN_DETAILS](state,action){
+        console.log('reducer - SET_LOGIN_DETAILS');
     return state+1;
   },
+
+
+    [types.SET_NAME](state,action){
+          console.log('reducer - SET_NAME');
+      return 'Max';
+    },
+
+})
+
+
+
+export const name = createReducer(0, {
+  
+
+    [types.SET_NAME](state,action){
+          console.log('reducer - SET_NAME');
+      return 'Max';
+    },
 
 })
