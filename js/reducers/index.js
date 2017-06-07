afffb531f308ea459ea0312bd90710092dd6450a
Simoncar <simoncar@gmail.com>
2017-06-07 17:32:37 +0800

    Updates to redux

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -1,10 +1,10 @@
 
 import { combineReducers } from 'redux';
 import * as loginReducer from './login';
-import * as userReducer from './user';
 
+import user from './user';
 import drawer from './drawer';
 
 export default combineReducers({
-  drawer, userReducer
+  drawer, user
 });
