d9bbac2e4954fd39ba2400a4b7faf06839b641a9
Simoncar <simoncar@gmail.com>
2017-06-10 17:01:19 +0800

    Persist rehydrate

--- js/configureStore.js
+++ js/configureStore.js
@@ -3,18 +3,19 @@ import { AsyncStorage } from 'react-native';
 import devTools from 'remote-redux-devtools';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { persistStore } from 'redux-persist';
+import { persistStore, autoRehydrate } from 'redux-persist';
 import reducer from './reducers';
 import promise from './promise';
 
 export default function configureStore(onCompletion:()=>void):any {
   const enhancer = compose(
     applyMiddleware(thunk, promise),
+    autoRehydrate(),
     devTools({
-      name: 'loggerWhatsOn', realtime: true,
+      name: '0001_sais_edu_sg', realtime: true,
     }),
   );
-
+   
   const store = createStore(reducer, enhancer);
   persistStore(store, { storage: AsyncStorage }, onCompletion);
 
