74a356f9d75f590dc3aab1a9dd0fd20a948351cb
Simoncar <simoncar@gmail.com>
2017-06-20 16:15:32 +0800

    Navigation cleanup

--- js/components/sideBar/index.js
+++ js/components/sideBar/index.js
@@ -38,13 +38,6 @@ class SideBar extends Component {
                 <Icon name="ios-grid" />
                 <Text style={styles.linkText}>myStamford</Text>
             </ListItem>
-            <ListItem button onPress={() => { Actions.webportal(); this.props.closeDrawer(); }} iconLeft style={styles.links} >
-                <Text style={styles.linkText}>  > Cafe TopUp</Text>
-            </ListItem>
-
-            <ListItem button onPress={() => { Actions.webportal(); this.props.closeDrawer(); }} iconLeft style={styles.links} >
-                <Text style={styles.linkText}>  > Events</Text>
-            </ListItem>
 
             <View style={styles.logoutContainer}>
               <View style={styles.logoutbtn} foregroundColor={'white'}>
