cc85deda3aca53931bec91f7f2c6ff58766c65c4
Simoncar <simoncar@gmail.com>
2017-06-21 23:01:53 +0800

    Digital Signage

---
+++ js/components/signage/index.js
@@ -0,0 +1,136 @@
+
+import React, { Component } from 'react';
+
+import {  WebView, Image, View, Platform } from 'react-native';
+
+import { Container, Header, Content, Text, Button, Icon, Left, Right, Body } from 'native-base';
+import { Grid, Col } from 'react-native-easy-grid';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'
+
+import { openDrawer } from '../../actions/drawer';
+
+import { ActionCreators } from '../../actions'
+
+import theme from '../../themes/base-theme';
+import styles from './styles';
+import {getUsername, getPassword} from '../global.js'
+
+const primary = require('../../themes/variable').brandPrimary;
+
+var WEBVIEW_REF = 'webview';
+var DEFAULT_URL = 'https://mystamford.edu.sg/login/login.aspx?prelogin=http%3a%2f%2fmystamford.edu.sg%2f&kr=iSAMS:ParentPP';
+
+var injectScript  = '';
+
+
+class Webportal extends Component {
+  static propTypes = {
+    openDrawer: React.PropTypes.func,
+    navigation: React.PropTypes.shape({
+      key: React.PropTypes.string,
+    }),
+  }
+
+
+
+  constructor(props) {
+    super(props);
+
+  }
+
+  state = {
+    url: DEFAULT_URL,
+    status: 'No Page Loaded',
+    backButtonEnabled: false,
+    forwardButtonEnabled: false,
+    loading: true,
+    scalesPageToFit: true,
+  };
+
+
+  render() {
+    return (
+      <Container>
+        <Image source={require('../../../images/glow2.png')} style={styles.container} >
+          <Header>
+            <Left>
+              <Button transparent onPress={this.props.openDrawer} >
+                <Icon active name="menu" />
+              </Button>
+            </Left>
+
+            <Body>
+              <Image source={require('../../../images/Header-Logo-White-0001.png')} style={styles.imageHeader} />
+            </Body>
+
+            <Right>
+              <Button transparent>
+                <Icon active name="ios-restaurant" onPress={this.pressGoButton}/>
+              </Button>
+
+            </Right>
+          </Header>
+
+          <WebView
+
+              source={{uri: this.state.url}}
+               javaScriptEnabled={true}
+               domStorageEnabled={true}
+               startInLoadingState={true}
+               ref={WEBVIEW_REF}
+             />
+
+        </Image>
+      </Container>
+    );
+  };
+
+
+
+return{
+
+}
+};
+
+  reload = () => {
+     this.refs[WEBVIEW_REF].reload();
+   };
+
+  pressGoButton = () => {
+      var url = 'http://mystamford.edu.sg/cafe/cafe-online-ordering#anchor';
+      if (url === this.state.url) {
+        this.reload();
+      } else {
+        this.setState({
+          url: url,
+        });
+      }
+    };
+}
+
+
+function bindAction(dispatch) {
+  return {
+    openDrawer: () => dispatch(openDrawer()),
+  };
+}
+
+function navigateCafe() {
+  return {
+    openDrawer: () => dispatch(openDrawer()),
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators (ActionCreators, dispatch);
+};
+
+const mapStateToProps = state => ({
+  navigation: state.cardNavigation,
+  userX: state.user,
+  passwordX: state.password
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Webportal);
