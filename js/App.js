8d6a046c1dfc9733120f02cb03fb71385f57e7a7
Simoncar <simoncar@gmail.com>
2017-07-13 21:55:12 +1000

    Push Notification

--- js/App.js
+++ js/App.js
@@ -3,21 +3,50 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as actionCreators  from './actions'
+import { Permissions, Notifications } from 'expo';
+
 
 import AppNavigator from './AppNavigator';
-
-
+import registerForPushNotificationsAsync from './lib/registerForPushNotificationsAsync';
 
 console.log("AC=", actionCreators);
 
 class App extends Component {
+  componentDidMount() {
+    this._notificationSubscription = this._registerForPushNotifications();
+  }
 
+  componentWillUnmount() {
+    this._notificationSubscription && this._notificationSubscription.remove();
+  }
   render() {
 
       console.log("_app.js in js folder");
 
     return <AppNavigator {...this.props}/>;
   }
+
+  _registerForPushNotifications() {
+    // Send our push token over to our backend so we can receive notifications
+    // You can comment the following line out if you want to stop receiving
+    // a notification every time you open the app. Check out the source
+    // for this function in api/registerForPushNotificationsAsync.js
+    registerForPushNotificationsAsync();
+
+    // Watch for incoming notifications
+    this._notificationSubscription = Notifications.addListener(
+      this._handleNotification
+    );
+  }
+
+  _handleNotification = ({ origin, data }) => {
+    console.log(
+      `Push notification ${origin} with data: ${JSON.stringify(data)}`
+    );
+  };
+
+
+
 }
 
 //export default App;
---
+++ js/lib/registerForPushNotificationsAsync.js
@@ -0,0 +1,35 @@
+import { Permissions, Notifications } from 'expo';
+
+// Example server, implemented in Rails: https://git.io/vKHKv
+//const PUSH_ENDPOINT = 'https://exponent-push-server.herokuapp.com/tokens';
+//const PUSH_ENDPOINT = 'https://mystamford.herokuapp.com/tokens';
+
+const PUSH_ENDPOINT = 'https://script.google.com/macros/s/AKfycbwhrlEfQhiSgcsF6AM_AlaMWxU7SsEtJ-yQpvthyQTT1jui588E/exec';
+
+export default (async function registerForPushNotificationsAsync() {
+  // Android remote notification permissions are granted during the app
+  // install, so this will only ask on iOS
+  let { status } = await Permissions.askAsync(Permissions.REMOTE_NOTIFICATIONS);
+
+  // Stop here if the user did not grant permissions
+  if (status !== 'granted') {
+    return;
+  }
+
+  // Get the token that uniquely identifies this device
+  let token = await Notifications.getExponentPushTokenAsync();
+
+  // POST the token to our backend so we can use it to send pushes from there
+  return fetch(PUSH_ENDPOINT + '?token=' + token, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      token: {
+        value: token,
+      },
+    }),
+  });
+});
