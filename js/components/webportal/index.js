85dd9456fdef18bfba044e061c81cb7b35258778
Simoncar <simoncar@gmail.com>
2017-07-20 09:48:58 +1000

    Hide settings message on web portal

--- js/components/webportal/index.js
+++ js/components/webportal/index.js
@@ -2,7 +2,7 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
-import {Dimensions, TouchableOpacity, WebView,ScrollView, Image, View, Platform } from 'react-native';
+import {TouchableHighlight,Animated, Dimensions, TouchableOpacity, WebView,ScrollView, Image, View, Platform } from 'react-native';
 import { Actions, ActionConst } from 'react-native-router-flux';
 
 import { Container, Header, Content, Text, Button, Icon, Left, Right, Body } from 'native-base';
@@ -20,18 +20,20 @@ import theme from '../../themes/base-theme';
 import styles from './styles';
 
 const primary = require('../../themes/variable').brandPrimary;
+const timer = require('react-native-timer');
+
 
 const headerLogo = require('../../../images/Header-Logo-White-0001.png');
-
 
 var WEBVIEW_REF = 'webview';
 var DEFAULT_URL = 'https://mystamford.edu.sg/login/login.aspx?prelogin=http%3a%2f%2fmystamford.edu.sg%2f&kr=iSAMS:ParentPP';
 
 var injectScript  = '';
 
-
 class Webportal extends Component {
+
   static propTypes = {
+
     openDrawer: PropTypes.func,
     navigation: PropTypes.shape({
       key: PropTypes.string,
@@ -67,6 +69,8 @@ class Webportal extends Component {
 
   }
 
+
+
   state = {
     url: DEFAULT_URL,
     status: 'No Page Loaded',
@@ -75,9 +79,22 @@ class Webportal extends Component {
     loading: true,
     scalesPageToFit: true,
     cookies    : {},
-    webViewUrl : ''
+    webViewUrl : '',
+    visible: this.props.visible,
+      myText: 'My Original Text',
+      showMsg: false
 
   };
+
+  componentWillUnmount() {
+    timer.clearTimeout(this);
+  }
+
+  showMsg() {
+    this.setState({showMsg: true}, () => timer.setTimeout(
+      this, 'hideMsg', () => this.setState({showMsg: false}), 2000
+    ));
+  }
 
   onNavigationStateChange = (webViewState: { url: string }) => {
 console.log ('webview = onNavigationStateChange');
@@ -88,6 +105,29 @@ console.log ('webview = onNavigationStateChange');
 //      this.setState({ webViewUrl: url })
   //  }
   }
+
+
+  componentWillMount() {
+    this._visibility = new Animated.Value(this.props.visible ? 1 : 0);
+
+    this.setState({showMsg: true}, () => timer.setTimeout(
+      this, 'hideMsg', () => this.setState({showMsg: false}), 5000
+    ));
+
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.visible) {
+      this.setState({ visible: true });
+    }
+    Animated.timing(this._visibility, {
+      toValue: nextProps.visible ? 1 : 0,
+      duration: 300,
+    }).start(() => {
+      this.setState({ visible: nextProps.visible });
+    });
+  }
+
 
   _checkNeededCookies = () => {
     console.log ('webview = _checkNeededCookies');
@@ -122,41 +162,88 @@ console.log ('webview = onNavigationStateChange');
        new_cookies[c[0]] = c[1];
 
        this.setState({ cookies: new_cookies });
-
-
-console.log ('     cookie = ', c);
+       console.log ('     cookie = ', c);
 
      });
 
      this._checkNeededCookies();
    }
 
+    toggleCancel () {
 
 
+        this.setState({
+            showCancel: !this.state.showCancel
+        });
+    }
+
+
+   _renderCancel () {
+        if (this.state.showMsg) {
+           return (
+              <TouchableOpacity onPress={() => Actions.login()}>
+               <View style={styles.settingsMessage} >
+
+                      <Grid>
+                      <Row>
+                        <Col style={{ width: 80 }}>
+                          <Button transparent style={styles.roundedButton}  onPress={() => Actions.login()} >
+                            <Icon name="settings" style={styles.settingsMessageIcon} />
+                          </Button>
+                        </Col>
+                        <Col>
+                            <Text style={styles.settingsMessageText}>Setup Username and Password</Text>
+                            <Text style={styles.settingsMessageText}>Logging in....</Text>
+                            </Col>
+                     </Row>
+                     </Grid>
+              </View>
+               </TouchableOpacity>
+           );
+       } else {
+          null;
+       }
+   }
+
+   updateText = () => {
+    this.setState({myText: 'My Changed Text'})
+ }
 
   render() {
+
+    _login =  () => {
+       console.log('here');
+       Animated.visible=false;
+       Animated.height = 0;
+       //this.toggleCancel();
+       this.state.showCancel = true;
+       this.setState({myText: 'My Changed Text'})
+    }
+
+    const { visible, style, children, ...rest } = this.props;
+
+    const containerStyle = {
+      opacity: this._visibility.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0, 1],
+      }),
+      transform: [
+        {
+          scale: this._visibility.interpolate({
+            inputRange: [0, 1],
+            outputRange: [1.1, 1],
+          }),
+        },
+      ],
+    };
+
+    const combinedStyle = [containerStyle, style];
+
     return (
       <ScrollView>
         <HeaderContent />
 
- <TouchableOpacity onPress={() => Actions.login()}>
-        <View style={styles.settingsMessage} >
-          <Grid>
-          <Row>
-            <Col style={{ width: 80 }}>
-              <Button transparent style={styles.roundedButton}  onPress={() => Actions.login()} >
-                <Icon name="settings" style={styles.settingsMessageIcon} />
-              </Button>
-            </Col>
-            <Col>
-                <Text style={styles.settingsMessageText}>Setup Username and Password</Text>
-                <Text style={styles.settingsMessageText}>on settings page</Text>
-            </Col>
-         </Row>
-         </Grid>
-
-</View>
-</TouchableOpacity>
+   {this._renderCancel()}
 
          <View  style={{height: 800}}>
         <Text>This is above the WebView.</Text>
