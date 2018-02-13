f070fa35ce37882f38cd5112d61a2aea91899ca5
Simoncar <simoncar@gmail.com>
2018-02-13 16:10:56 +0800

    Cleanup tennanting

--- js/components/sideBar/index.js
+++ js/components/sideBar/index.js
@@ -8,8 +8,10 @@ import { Actions, ActionConst } from 'react-native-router-flux';
 import { Container, Content, Text, Icon, List, ListItem, Thumbnail } from 'native-base';
 import { Grid, Col } from 'react-native-easy-grid';
 import { closeDrawer } from '../../actions/drawer';
-
+import { Constants } from 'expo';
 import styles from './style';
+
+var instID = Constants.manifest.extra.instance;
 
 class SideBar extends Component {
 
@@ -43,39 +45,36 @@ class SideBar extends Component {
 
             <ListItem button onPress={() => { Actions.webportal(); this.props.closeDrawer(); }} iconLeft style={styles.links} >
                 <Icon name="ios-grid" />
-                <Text style={styles.linkText}>MYSTAMFORD</Text>
+                <Text style={styles.linkText}>{global.switch_portalName}</Text>
             </ListItem>
 
-            <ListItem button onPress={() => { Actions.webportalSports(); this.props.closeDrawer(); }} iconLeft style={styles.links} >
-                <Icon name="ios-football-outline" />
-                <Text style={styles.linkText}>ATHLETICS</Text>
-            </ListItem>
+       {instID == '0001-sais_edu_sg' &&
+          <View>
+                <ListItem button onPress={() => { Actions.webportalSports(); this.props.closeDrawer(); }} iconLeft style={styles.links} >
+                    <Icon name="ios-football-outline" />
+                    <Text style={styles.linkText}>ATHLETICS</Text>
+                </ListItem>
+                <ListItem button onPress={() => { Actions.ptaHome(); this.props.closeDrawer(); }} iconLeft style={styles.links} >
+                    <Icon name="ios-people-outline" />
+                    <Text style={styles.linkText} >PTA</Text>
+                </ListItem>
+                <ListItem button onPress={() => { Actions.login(); this.props.closeDrawer(); }} iconLeft style={styles.links} >
+                    <Icon name="settings" />
+                    <Text style={styles.linkText}>SETTINGS</Text>
+                </ListItem>
+                            
 
-              <ListItem button onPress={() => { Actions.ptaHome(); this.props.closeDrawer(); }} iconLeft style={styles.links} >
-                  <Icon name="ios-people-outline" />
-                  <Text style={styles.linkText} >PTA</Text>
-              </ListItem>
+                <View style={styles.logoutContainer}>
+                  <View style={styles.logoutbtn} foregroundColor={'white'}>
+                      <Image
+                          style={{width: 96, height: 128}}
+                          source={require('../../../images/sais.edu.sg/pta_logo.png')}
+                      />
+                  </View>
+                </View>
+          </View>
+       }
 
-
-            <ListItem button onPress={() => { Actions.login(); this.props.closeDrawer(); }} iconLeft style={styles.links} >
-                <Icon name="settings" />
-                <Text style={styles.linkText}>SETTINGS</Text>
-            </ListItem>
-
-            <View style={styles.logoutContainer}>
-
-              <View style={styles.logoutbtn} foregroundColor={'white'}>
-
-                  <Image
-                      style={{width: 96, height: 128}}
-                      source={require('../../../images/sais.edu.sg/pta_logo.png')}
-                  />
-
-
-              </View>
-
-
-            </View>
           </Content>
           </ImageBackground>
       </Container>
