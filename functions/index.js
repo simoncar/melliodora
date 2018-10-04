04bb7687596c0bf2bb2e207eadcce208e90b99de
Simoncar <simoncar@gmail.com>
2018-10-04 10:52:38 +0800

    Push notification to hardcoded phone

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,46 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+var fetch = require('node-fetch')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+admin.initializeApp(functions.config().firebase);
+
+//send the push notification 
+exports.sendPushNotification = functions.database.ref('contacts/').onCreate((snap, context) => {
+
+    const createdData = snap.val(); 
+
+
+
+    var messages = []
+
+    messages.push({
+        "to": "ExponentPushToken[_fADI_E3xWnqNKWxZurGun]",
+        "sound": "default",
+        "body": "Simon says: Put your hands on your head"
+    });
+
+    //return the main promise 
+  
+        
+    
+        return Promise.all(messages)
+
+        .then(messages => {
+            // console.log(messages)
+            fetch('https://exp.host/--/api/v2/push/send', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(messages)
+
+            });
+        })
+        .catch(reason => {
+            console.log(reason)
+        })
+
+
+});
