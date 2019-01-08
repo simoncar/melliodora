ff1db63fab4508b92dec62b9cf2861106488ab7c
Simoncar <simoncar@gmail.com>
2019-01-08 11:33:00 +0900

    Firebase version bump

--- js/lib/firebase.js
+++ js/lib/firebase.js
@@ -17,7 +17,9 @@ class Firebase {
                 apiKey: "AIzaSyAbCADtQsj1lTQWD1pfaOMi-WHUGkRFTXw",
                 authDomain: "calendar-app-57e88.firebaseapp.com",
                 databaseURL: "https://calendar-app-57e88.firebaseio.com",
-                storageBucket: "calendar-app-57e88.appspot.com"
+                projectId: "calendar-app-57e88",
+                storageBucket: "calendar-app-57e88.appspot.com",
+                messagingSenderId: "991350571487"
             });
         }
 
