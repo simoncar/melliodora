beb3865b460128d872d688e0d708f2a002074569
Simoncar <simoncar@gmail.com>
2019-01-22 20:50:53 +0800

    Add Button

--- js/components/homeNav/styles.js
+++ js/components/homeNav/styles.js
@@ -31,7 +31,26 @@ export default {
     textAlign: 'center',
         fontSize: 12,
   },
-
+  addButton: {
+    backgroundColor: '#ff5722',
+    borderColor: '#ff5722',
+    borderWidth: 1,
+    height: 50,
+    width: 50,
+    borderRadius: 50 / 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    shadowColor: "#000000",
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    shadowOffset: {
+      height: 1,
+      width: 0,
+    },
+  },
 
   
   betaView: {
