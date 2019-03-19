72fbb8913503c78c8626e4f5e23cf6cfe6165079
Simoncar <simoncar@gmail.com>
2019-03-19 11:08:12 +0800

    Cleanup

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-var fetch = require('node-fetch');
+let fetch = require('node-fetch');
 
 admin.initializeApp(functions.config().firebase);
 
@@ -8,134 +8,125 @@ admin.initializeApp(functions.config().firebase);
 // firebase deploy
 // send the push notification
 exports.sendPushNotification = functions.database.ref('instance/0001-sais_edu_sg/chat/chatroom/International Fiesta/{createdID}').onCreate((snap, context) => {
-
   const createdData = snap.val();
-  console.log(createdData)
-  console.log(createdData.user.name + ': ' + createdData.text)
+  console.log(createdData);
+  console.log(`${createdData.user.name  }: ${  createdData.text}`);
 
-  var messages = []
+  let messages = [];
 
-  //cx iPhone
-    messages.push({
-        "to": "ExponentPushToken[0_rniBFtyWshjEYnkzcQXF]",
-        "title": "International Fiesta > " + createdData.user.name,
-        "sound": "default",
-        "body": createdData.text
+  // cx iPhone
+  messages.push({
+    'to': 'ExponentPushToken[0_rniBFtyWshjEYnkzcQXF]',
+    'title': 'International Fiesta > ' + createdData.user.name,
+    sound: 'default',
+    body: createdData.text,
+  });
+
+  // Android
+  messages.push({
+    to: 'ExponentPushToken[rXb2wQB9LjaLe3JlSarOsk]',
+    'title': 'International Fiesta > ' + createdData.user.name,
+    'sound': 'default',
+    body: createdData.text,
+  });
+
+  // simon iPhone
+  messages.push({
+    to: 'ExponentPushToken[lBKUcgCJqZDVFJALPCsq07]',
+    'title': 'International Fiesta > ' + createdData.user.name,
+    'sound': 'default',
+    body: createdData.text,
+  });
+
+  // return the main promise
+  return Promise.all(messages)
+
+    .then((messages) => {
+      // console.log(messages)
+      fetch('https://exp.host/--/api/v2/push/send', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(messages),
+
+      });
+    })
+    .catch((reason) => {
+      console.log(reason);
     });
-
-    // Android
-    messages.push({
-        "to": "ExponentPushToken[rXb2wQB9LjaLe3JlSarOsk]",
-        "title": "International Fiesta > " + createdData.user.name,
-        "sound": "default",
-        "body": createdData.text
-    });
-
-    //simon iPhone
-    messages.push({
-        "to": "ExponentPushToken[lBKUcgCJqZDVFJALPCsq07]",
-        "title": "International Fiesta > " + createdData.user.name,
-        "sound": "default",
-        "body": createdData.text
-    });
-
-    //return the main promise 
-    return Promise.all(messages)
-
-        .then(messages => {
-            // console.log(messages)
-            fetch('https://exp.host/--/api/v2/push/send', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(messages)
-
-            });
-        })
-        .catch(reason => {
-            console.log(reason)
-        })
-
 });
 
 
-exports.sendPushNotificationSimonAll = functions.database.ref('instance/0001-sais_edu_sg/chat/chatroom/Lost and Found/{createdID}').onCreate((snap, context) => {
+exports.sendPushNotificationSimonAll = functions.database.ref('instance/0001-sais_edu_sg/chat/chatroom/{createdID}').onCreate((snap, context) => {
+  const createdData = snap.val();
+  console.log(createdData);
+  
 
-    const createdData = snap.val();
-    console.log(createdData)
-    console.log(createdData.user.name + ': ' + createdData.text)
-  
-    var messages = []
-  
-  
-  
-      //simon iPhone
-      messages.push({
-          "to": "ExponentPushToken[lBKUcgCJqZDVFJALPCsq07]",
-          "title": "ALL > " + createdData.user.name,
-          "sound": "default",
-          "body": createdData.text
-      });
-  
-      //return the main promise 
-      return Promise.all(messages)
-  
-          .then(messages => {
-              // console.log(messages)
-              fetch('https://exp.host/--/api/v2/push/send', {
-                  method: 'POST',
-                  headers: {
-                      'Accept': 'application/json',
-                      'Content-Type': 'application/json',
-                  },
-                  body: JSON.stringify(messages)
-  
-              });
-          })
-          .catch(reason => {
-              console.log(reason)
-          })
-  
+  let messages = [];
+  // simon iPhone
+  messages.push({
+    to: 'ExponentPushToken[lBKUcgCJqZDVFJALPCsq07]',
+    title: 'New Chat Message in SAIS App',
+    'sound': 'default',
+    'body': createdData.text,
   });
-  
-  exports.sendPushNotificationGala = functions.database.ref('instance/0001-sais_edu_sg/chat/chatroom/Stamford 10 Year Gala/{createdID}').onCreate((snap, context) => {
 
-    const createdData = snap.val();
-    console.log(createdData)
-    console.log(createdData.user.name + ': ' + createdData.text)
-  
-    var messages = []
-  
-  
-  
-      //simon iPhone
-      messages.push({
-          "to": "ExponentPushToken[lBKUcgCJqZDVFJALPCsq07]",
-          "title": "ALL > " + createdData.user.name,
-          "sound": "default",
-          "body": createdData.text
+  // return the main promise
+  return Promise.all(messages)
+
+    .then((messages) => {
+      // console.log(messages)
+      fetch('https://exp.host/--/api/v2/push/send', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(messages),
+
       });
-  
-      //return the main promise 
-      return Promise.all(messages)
-  
-          .then(messages => {
-              // console.log(messages)
-              fetch('https://exp.host/--/api/v2/push/send', {
-                  method: 'POST',
-                  headers: {
-                      'Accept': 'application/json',
-                      'Content-Type': 'application/json',
-                  },
-                  body: JSON.stringify(messages)
-  
-              });
-          })
-          .catch(reason => {
-              console.log(reason)
-          })
-  
+    })
+    .catch((reason) => {
+      console.log(reason);
+    });
+});
+
+exports.sendPushNotificationGala = functions.database.ref('instance/0001-sais_edu_sg/chat/chatroom/Stamford 10 Year Gala/{createdID}').onCreate((snap, context) => {
+  const createdData = snap.val();
+  console.log(createdData);
+  console.log(`${createdData.user.name  }: ${  createdData.text}`);
+
+  let messages = [];
+
+
+
+  // simon iPhone
+  messages.push({
+    to: 'ExponentPushToken[lBKUcgCJqZDVFJALPCsq07]',
+    title: 'ALL > ' + createdData.user.name,
+    sound: 'default',
+    'body': createdData.text,
   });
-  
+
+  // return the main promise
+  return Promise.all(messages)
+
+    .then((messages) => {
+      // console.log(messages)
+      fetch('https://exp.host/--/api/v2/push/send', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(messages),
+
+      });
+    })
+    .catch((reason) => {
+      console.log(reason);
+    });
+});
+
--- js/components/chat/index.js
+++ js/components/chat/index.js
@@ -1,118 +1,121 @@
 import React, { Component } from 'react';
-import { Platform, Text, View, Alert } from 'react-native';
+import {
+  Platform, Text, View, Alert,
+} from 'react-native';
 import { connect } from 'react-redux';
 import { Actions as NavigationActions } from 'react-native-router-flux';
-import { GiftedChat, Actions, Bubble, SystemMessage, Time } from 'react-native-gifted-chat';
-import { Container, Header, Footer, Button, Icon, Body } from 'native-base';
+import {
+  GiftedChat, Actions, Bubble, SystemMessage, Time,
+} from 'react-native-gifted-chat';
+import {
+  Container, Header, Footer, Button, Icon, Body,
+} from 'native-base';
 import emojiUtils from 'emoji-utils';
+import Constants from 'expo';
+import { bindActionCreators } from 'redux';
 import CustomActions from './customActions';
 import CustomView from './customView';
 import styles from './styles';
 import HeaderContent from './../headerContent/header/';
-import Constants from 'expo';
 
-import { bindActionCreators } from 'redux';
 import * as ActionCreators from '../../actions';
 
 import Backend from './backend';
 import SlackMessage from './slackMessage';
 
 class chat extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+      loadEarlier: true,
+      typingText: null,
+      isLoadingEarlier: false,
+      step: 0,
+    };
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            messages: [],
-            loadEarlier: true,
-            typingText: null,
-            isLoadingEarlier: false,
-            step: 0,
-        };
+    this._isMounted = false;
+    this.onSend = this.onSend.bind(this);
+    this.parsePatterns = this.parsePatterns.bind(this);
 
-        this._isMounted = false;
-        this.onSend = this.onSend.bind(this);
-        this.parsePatterns = this.parsePatterns.bind(this);
+    this.onReceive = this.onReceive.bind(this);
+    this.renderCustomActions = this.renderCustomActions.bind(this);
+    this.renderBubble = this.renderBubble.bind(this);
+    this.renderSystemMessage = this.renderSystemMessage.bind(this);
+    this.renderFooter = this.renderFooter.bind(this);
+    this.onLoadEarlier = this.onLoadEarlier.bind(this);
 
-        this.onReceive = this.onReceive.bind(this);
-        this.renderCustomActions = this.renderCustomActions.bind(this);
-        this.renderBubble = this.renderBubble.bind(this);
-        this.renderSystemMessage = this.renderSystemMessage.bind(this);
-        this.renderFooter = this.renderFooter.bind(this);
-        this.onLoadEarlier = this.onLoadEarlier.bind(this);
+    this._isAlright = null;
+  }
 
-        this._isAlright = null;
-    }
+  componentWillMount() {
+    if (this.props.userX.nickname) {
+      // we have a value, good
 
-    componentWillMount() {
-
-        if (this.props.userX.nickname) {
-            //we have a value, good
-
-        } else {
-            this.noNickname();
-            NavigationActions.login();
-        };
-
-
-        this._isMounted = true;
-    }
-
-    componentDidMount() {
-        Backend.setChatroom(this.props.chatroom);
-
-        Backend.loadMessages((message) => {
-            this.setState(previousState => ({
-                messages: GiftedChat.append(previousState.messages, message),
-            }));
-        });
-    }
-
-    componentWillUnmount() {
-        this._isMounted = false;
-        Backend.closeChat();
+    } else {
+      this.noNickname();
+      NavigationActions.login();
     }
 
 
-    noNickname() {
-        Alert.alert(
-            'Chat Name',
-            'Please enter a Name to Chat',
-            [
+    this._isMounted = true;
+  }
 
-                { text: 'OK', onPress: () => console.log('OK Pressed') },
-            ],
-            { cancelable: false },
-        );
-    }
+  componentDidMount() {
+    Backend.setChatroom(this.props.chatroom);
 
-    avatarPress = props => {
+    Backend.loadMessages((message) => {
+      this.setState(previousState => ({
+        messages: GiftedChat.append(previousState.messages, message),
+      }));
+    });
+  }
 
-        console.log(props._id)
-        console.log(props.name)
+  componentWillUnmount() {
+    this._isMounted = false;
+    Backend.closeChat();
+  }
 
-        Alert.alert(
-            props.name,
-        );
+
+  noNickname() {
+    Alert.alert(
+      'Chat Name',
+      'Please enter a Name to Chat',
+      [
+
+        { text: 'OK', onPress: () => console.log('OK Pressed') },
+      ],
+      { cancelable: false },
+    );
+  }
+
+    avatarPress = (props) => {
+      console.log(props._id);
+      console.log(props.name);
+
+      Alert.alert(
+        props.name,
+      );
     };
 
     onLoadEarlier() {
-        this.setState(previousState => ({
-            isLoadingEarlier: true,
-        }));
+      this.setState(previousState => ({
+        isLoadingEarlier: true,
+      }));
 
-        setTimeout(() => {
-            if (this._isMounted === true) {
-                this.setState(previousState => ({
-                    messages: GiftedChat.prepend(previousState.messages, require('./old_messages.js')),
-                    loadEarlier: false,
-                    isLoadingEarlier: false,
-                }));
-            }
-        }, 2000); // simulating network
+      setTimeout(() => {
+        if (this._isMounted === true) {
+          this.setState(previousState => ({
+            messages: GiftedChat.prepend(previousState.messages, require('./old_messages.js')),
+            loadEarlier: false,
+            isLoadingEarlier: false,
+          }));
+        }
+      }, 2000); // simulating network
     }
 
     onSend(messages = []) {
-        Backend.SendMessage(messages);
+      Backend.SendMessage(messages);
     }
 
     onReceive(text) {
@@ -120,269 +123,268 @@ class chat extends Component {
     }
 
     renderCustomActions(props) {
-        if (Platform.OS === 'ios') {
-            return (
-                <CustomActions
-                    {...props}
-                />
-            );
-        }
-        const options = {
-            'Action 1': (props) => {
-                alert('option 1');
-            },
-            'Action 2': (props) => {
-                alert('option 2');
-            },
-            Cancel: () => { },
-        };
+      if (Platform.OS === 'ios') {
         return (
-            <Actions
-                {...props}
-                options={options}
-            />
+          <CustomActions
+            {...props}
+          />
         );
+      }
+      const options = {
+        'Action 1': (props) => {
+          alert('option 1');
+        },
+        'Action 2': (props) => {
+          alert('option 2');
+        },
+        Cancel: () => { },
+      };
+      return (
+        <Actions
+          {...props}
+          options={options}
+        />
+      );
     }
 
 
     renderSystemMessage(props) {
-        return (
-            <SystemMessage
-                {...props}
-                containerStyle={{
-                    marginBottom: 15,
-                }}
-                textStyle={{
-                    fontSize: 14,
-                }}
-            />
-        );
+      return (
+        <SystemMessage
+          {...props}
+          containerStyle={{
+            marginBottom: 15,
+          }}
+          textStyle={{
+            fontSize: 14,
+          }}
+        />
+      );
     }
 
     renderCustomView(props) {
-        return (
-            <CustomView
-                {...props}
-            />
-        );
+      return (
+        <CustomView
+          {...props}
+        />
+      );
     }
 
     renderFooter(props) {
-        if (this.state.typingText) {
-            return (
-                <View style={styles.footerContainer}>
-                    <Text style={styles.footerText}>
-                        {this.state.typingText}
-                    </Text>
-                </View>
-            );
-        }
-        return null;
+      if (this.state.typingText) {
+        return (
+          <View style={styles.footerContainer}>
+            <Text style={styles.footerText}>
+              {this.state.typingText}
+            </Text>
+          </View>
+        );
+      }
+      return null;
     }
 
     renderMessage(props) {
-        const { currentMessage: { text: currText } } = props;
+      const { currentMessage: { text: currText } } = props;
 
-        let messageTextStyle;
+      let messageTextStyle;
 
-        // Make "pure emoji" messages much bigger than plain text.
-        if (currText && emojiUtils.isPureEmojiString(currText)) {
-            messageTextStyle = {
-                fontSize: 28,
-                // Emoji get clipped if lineHeight isn't increased; make it consistent across platforms.
-                lineHeight: Platform.OS === 'android' ? 34 : 30,
-            };
-        }
+      // Make "pure emoji" messages much bigger than plain text.
+      if (currText && emojiUtils.isPureEmojiString(currText)) {
+        messageTextStyle = {
+          fontSize: 28,
+          // Emoji get clipped if lineHeight isn't increased; make it consistent across platforms.
+          lineHeight: Platform.OS === 'android' ? 34 : 30,
+        };
+      }
 
-        return (
-            <SlackMessage {...props} messageTextStyle={messageTextStyle} />
-        );
+      return (
+        <SlackMessage {...props} messageTextStyle={messageTextStyle} />
+      );
     }
 
     renderBubble3(props) {
-        return (
-            <Bubble
-                {...props}
-                wrapperStyle={{
-                    left: {
-                        backgroundColor: '#f0f0f0',
-                    }
-                }}
-            />
-        );
+      return (
+        <Bubble
+          {...props}
+          wrapperStyle={{
+            left: {
+              backgroundColor: '#f0f0f0',
+            },
+          }}
+        />
+      );
     }
 
     renderBubble2 = (props) => {
-        const username = this.props.userX.nickname;
-        const color = this.getColor(username);
+      const username = this.props.userX.nickname;
+      const color = this.getColor(username);
 
-        return (
+      return (
 
-            <Bubble
-                {...props}
-                textStyle={{
-                    right: {
-                        color: 'white',
-                    },
-                }}
-                wrapperStyle={{
-                    left: {
-                        backgroundColor: color,
-                    },
-                }}
-            />
-        );
+        <Bubble
+          {...props}
+          textStyle={{
+            right: {
+              color: 'white',
+            },
+          }}
+          wrapperStyle={{
+            left: {
+              backgroundColor: color,
+            },
+          }}
+        />
+      );
     }
 
     get user() {
-        // Return our name and our UID for GiftedChat to parse
+      // Return our name and our UID for GiftedChat to parse
 
-        return {
-            name: this.props.userX.nickname,
-            _id: Expo.Constants.installationId,
-        };
+      return {
+        name: this.props.userX.nickname,
+        _id: Expo.Constants.installationId,
+      };
     }
 
     getColor(username) {
-        let sumChars = 0;
-        for (let i = 0; i < username.length; i++) {
-            sumChars += username.charCodeAt(i);
-        }
+      let sumChars = 0;
+      for (let i = 0; i < username.length; i++) {
+        sumChars += username.charCodeAt(i);
+      }
 
-        const colors = [
-            '#d6cfc7', // carrot
-            '#c7c6c1', // emerald
-            '#bebdb8', // peter  river
-            '#bdb7ab', // wisteria
-            '#d9dddc', // alizarin
-            '#b9bbb6', // turquoise
-            '#808588', // midnight blue
-        ];
-        return colors[sumChars % colors.length];
+      const colors = [
+        '#d6cfc7', // carrot
+        '#c7c6c1', // emerald
+        '#bebdb8', // peter  river
+        '#bdb7ab', // wisteria
+        '#d9dddc', // alizarin
+        '#b9bbb6', // turquoise
+        '#808588', // midnight blue
+      ];
+      return colors[sumChars % colors.length];
     }
 
     parsePatterns(linkStyle) {
-        return [
-            {
-                pattern: /#(\w+)/,
-                style: { ...linkStyle, color: 'orange' },
-                onPress: () => Linking.openURL('http://gifted.chat'),
-            },
-        ];
+      return [
+        {
+          pattern: /#(\w+)/,
+          style: { ...linkStyle, color: 'orange' },
+          onPress: () => Linking.openURL('http://gifted.chat'),
+        },
+      ];
     }
 
     /** render the chat bubble */
     renderBubble(props) {
-        return (
-            <Bubble
-                {...props}
-                wrapperStyle={{
-                    left: {
-                        backgroundColor: 'pink',
-                    },
-                    right: {
-                        backgroundColor: 'blue',
-                    }
-                }}
-                textStyle={{
-                    right: {
-                        color: 'white',
-                        //fontFamily: 'Montserrat-Light',
-                        fontSize: 14
-                    },
-                    left: {
-                        color: 'black',
-                        //fontFamily: 'Montserrat-Light',
-                        fontSize: 14
-                    }
-                }}
-            />
-        );
+      return (
+        <Bubble
+          {...props}
+          wrapperStyle={{
+            left: {
+              backgroundColor: 'pink',
+            },
+            right: {
+              backgroundColor: 'blue',
+            },
+          }}
+          textStyle={{
+            right: {
+              color: 'white',
+              // fontFamily: 'Montserrat-Light',
+              fontSize: 14,
+            },
+            left: {
+              color: 'black',
+              // fontFamily: 'Montserrat-Light',
+              fontSize: 14,
+            },
+          }}
+        />
+      );
     }
 
     /** render the time labels in the bubble */
     renderTime() {
-        return (
-            <Time
-                textStyle={{
-                    right: {
-                        color: 'blue',
-                        //fontFamily: 'Montserrat-Light',
-                        fontSize: 14
-                    },
-                    left: {
-                        color: 'green',
-                        //fontFamily: 'Montserrat-Light',
-                        fontSize: 14
-                    }
-                }}
-            />
-        );
+      return (
+        <Time
+          textStyle={{
+            right: {
+              color: 'blue',
+              // fontFamily: 'Montserrat-Light',
+              fontSize: 14,
+            },
+            left: {
+              color: 'green',
+              // fontFamily: 'Montserrat-Light',
+              fontSize: 14,
+            },
+          }}
+        />
+      );
     }
 
     render() {
-        return (
-            <Container>
+      return (
+        <Container>
 
-                <HeaderContent
-                    showBack
-                />
+          <HeaderContent
+            showBack
+          />
 
-<Text style={styles.chatHeading}>{this.props.chatroom}</Text>
+          <Text style={styles.chatHeading}>{this.props.chatroom}</Text>
 
-                <GiftedChat
-                    messages={this.state.messages}
-                    onSend={this.onSend}
+          <GiftedChat
+            messages={this.state.messages}
+            onSend={this.onSend}
                     // loadEarlier={this.state.loadEarlier}
                     // onLoadEarlier={this.onLoadEarlier}
                     // isLoadingEarlier={this.state.isLoadingEarlier}
 
-                    user={{
-                        _id: Expo.Constants.installationId,  //`${Constants.installationId}${Constants.deviceId}`, // sent messages should have same user._id
-                        name: this.props.userX.nickname,
-                        //avatar: 'https://www.sais.edu.sg/sites/all/themes/custom/saissg/favicon.ico',
-                    }}
+            user={{
+                _id: Expo.Constants.installationId, // `${Constants.installationId}${Constants.deviceId}`, // sent messages should have same user._id
+                name: this.props.userX.nickname,
+                // avatar: 'https://www.sais.edu.sg/sites/all/themes/custom/saissg/favicon.ico',
+              }}
 
-                    //renderActions={this.renderCustomActions}
-                    //renderBubble={this.renderBubble}
+                    // renderActions={this.renderCustomActions}
+                    // renderBubble={this.renderBubble}
                     // renderSystemMessage={this.renderSystemMessage}
-                    renderCustomView={this.renderCustomView}
+            renderCustomView={this.renderCustomView}
                     // renderFooter={this.renderFooter}
                     // showAvatarForEveryMessage
                     // showUserAvatar
                     // parsePatterns={this.parsePatterns}
-                    //renderMessage={this.renderBubble}   
+                    // renderMessage={this.renderBubble}
 
-                    //renderBubble={this.renderBubble.bind(this)}
-                    //renderAvatar={this.renderAvatar.bind(this)}
-                    //renderTime={this.renderTime.bind(this)}
-                    showUserAvatar={true}
-                    //showAvatarForEveryMessage={true}
-                    chatId={this.chatId}
-                    //minInputToolbarHeight={50}
-                    bottomOffset={0}
-                    onPressAvatar={this.avatarPress}
-                />
+                    // renderBubble={this.renderBubble.bind(this)}
+                    // renderAvatar={this.renderAvatar.bind(this)}
+                    // renderTime={this.renderTime.bind(this)}
+            showUserAvatar
+                    // showAvatarForEveryMessage={true}
+            chatId={this.chatId}
+                    // minInputToolbarHeight={50}
+            bottomOffset={0}
+            onPressAvatar={this.avatarPress}
+          />
 
-                <Footer style={styles.footer} />
-            </Container>
-        );
+          <Footer style={styles.footer} />
+        </Container>
+      );
     }
 }
 
 function bindAction(dispatch) {
-    return {
-        openDrawer: () => dispatch(openDrawer()),
-    };
+  return {
+    openDrawer: () => dispatch(openDrawer()),
+  };
 }
 
 const mapDispatchToProps = dispatch => bindActionCreators(ActionCreators, dispatch);
 
 const mapStateToProps = state => ({
-    navigation: state.cardNavigation,
-    username: state.username,
-    userX: state.user,
+  navigation: state.cardNavigation,
+  username: state.username,
+  userX: state.user,
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(chat);
-
