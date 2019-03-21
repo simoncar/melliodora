a5acf446834a10a6f5ee29fa1fb28648ce9a5ce3
Simoncar <simoncar@gmail.com>
2019-03-21 15:41:47 +0800

    Cleanup

--- js/components/homeNav/index.js
+++ js/components/homeNav/index.js
@@ -2,37 +2,39 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Image, ListView, FlatList, View, Linking, TouchableOpacity, TouchableHighlight, StyleSheet, Dimensions } from 'react-native';
+import {
+  Image, ListView, FlatList, View, Linking, TouchableOpacity, TouchableHighlight, StyleSheet, Dimensions,
+} from 'react-native';
 import { Actions } from 'react-native-router-flux';
-import { Container, Content, Text, Icon, Button } from 'native-base';
+import {
+  Container, Content, Text, Icon, Button,
+} from 'native-base';
 import { Grid, Col, Row } from 'react-native-easy-grid';
 import { Expo, Constants, Notifications } from 'expo';
 import moment from 'moment';
 import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import * as firebase from 'firebase';
 import { isAdmin } from '../global.js';
 
-
 import * as ActionCreators from '../../actions';
-import HeaderContent from './../headerContent/header';
+import HeaderContent from '../headerContent/header';
 
 import { openDrawer } from '../../actions/drawer';
 
 import styles from './styles';
 
-import * as firebase from 'firebase';
-
 const { width } = Dimensions.get('window');
 
-var calendarEvents = [];
+const calendarEvents = [];
 
 const ListItem = require('./ListItem');
-var instID = Constants.manifest.extra.instance;
+
+const instID = Constants.manifest.extra.instance;
 
 const token = Notifications.getExpoPushTokenAsync();
 console.log(token);
 
 class HomeNav extends Component {
-
   static propTypes = {
 
     openDrawer: PropTypes.func,
@@ -43,28 +45,23 @@ class HomeNav extends Component {
   }
 
   constructor(props) {
-    super(props)
+    super(props);
 
-    this.calendarEvents = firebase.database().ref('instance/' + instID + '/feature');
+    this.calendarEvents = firebase.database().ref(`instance/${instID}/feature`);
     this.state = {
-      versionText: '', //'Version Aug.1.2017 - Check for an Update'
+      versionText: '', // 'Version Aug.1.2017 - Check for an Update'
       calendarEvents: new ListView.DataSource({
         rowHasChanged: (row1, row2) => row1 !== row2,
-      })
-    }
-
-
-    console.log("D= ", Constants.installationId)
+      }),
+    };
   }
-
-
 
   componentDidMount() {
     this.listenLoadFromFirebase(this.calendarEvents);
   }
 
-  keyExtractor = (item) => item._key;
-  
+  keyExtractor = item => item._key;
+
   static propTypes = {
     navigation: PropTypes.shape({
       key: PropTypes.string,
@@ -73,27 +70,24 @@ class HomeNav extends Component {
 
   _handleOpenWithLinking = (sURL) => {
     Linking.openURL(sURL);
- }
+  }
 
   listenLoadFromFirebase(calendarEvents) {
-
     calendarEvents.on('value', (snap) => {
-
-      var items = [];
-      var itemsHidden = [];
-      var today = new moment().format();;
+      const items = [];
+      const itemsHidden = [];
+      const today = new moment().format();
 
       snap.forEach((child) => {
-           
-        var displayStart = (child.val().displayStart !== undefined) ? moment().format(child.val().displayStart) : null;
-        var displayEnd =(child.val().displayEnd !== undefined) ? moment().format(child.val().displayEnd) : null;
-        var hidden = true;
+        const displayStart = (child.val().displayStart !== undefined) ? moment().format(child.val().displayStart) : null;
+        const displayEnd = (child.val().displayEnd !== undefined) ? moment().format(child.val().displayEnd) : null;
+        let hidden = true;
 
         if (displayStart != null && displayStart <= today) {
-          //start is less than End 
+          // start is less than End
 
           if (displayStart != null && displayEnd >= today) {
-            //start is less than End 
+            // start is less than End
             hidden = false;
           }
         }
@@ -141,16 +135,13 @@ class HomeNav extends Component {
             _key: child.key,
           });
         }
-
       });
 
       this.setState({
         calendarEvents: items,
         calendarEventsHidden: itemsHidden,
       });
-
     });
-
   }
 
   _renderItem(item) {
@@ -159,19 +150,18 @@ class HomeNav extends Component {
     );
   }
 
-
-  _renderItem23 = ({ item }) =>
-    <View >
+  _renderItem23 = ({ item }) => (
+    <View>
       <Text style={{ color: 'blue' }}>{item.title}</Text>
-    </View>;
-
+    </View>
+  );
 
   render() {
     return (
       <Container>
 
         <HeaderContent
-          showHome='false'
+          showHome="false"
         />
 
         <Content showsVerticalScrollIndicator={false}>
@@ -179,126 +169,147 @@ class HomeNav extends Component {
             <Grid>
 
               <Row style={{ paddingTop: 20, paddingBottom: 20 }}>
-              <Col>
-                    <Button transparent style={styles.roundedButton} onPress={() => { Actions.webportalSports(); }} >
-                      <MaterialCommunityIcons style={styles.icon} name="soccer" />
-                    </Button>
-                    <Text note style={styles.buttonLabel}>Athletics</Text>
-                  </Col>
                 <Col>
-                  <Button transparent style={styles.roundedButton} onPress={() => { Actions.home(); }} >
+                  <Button transparent style={styles.roundedButton} onPress={() => { Actions.webportalSports(); }}>
+                    <MaterialCommunityIcons style={styles.icon} name="soccer" />
+                  </Button>
+                  <Text note style={styles.buttonLabel}>Athletics</Text>
+                </Col>
+                <Col>
+                  <Button transparent style={styles.roundedButton} onPress={() => { Actions.home(); }}>
                     <Icon style={styles.icon} name="ios-calendar" />
                   </Button>
                   <Text note style={styles.buttonLabel}>Calendar</Text>
                 </Col>
                 <Col>
-                  <Button transparent style={styles.roundedButton} onPress={() => { Actions.webportal(); }} >
+                  <Button transparent style={styles.roundedButton} onPress={() => { Actions.webportal(); }}>
                     <Icon style={styles.icon} name="ios-grid" />
                   </Button>
                   <View style={{ flex: 1, flexDirection: 'row', alignSelf: 'center' }}>
 
                     <Icon style={styles.iconLabel} name="ios-lock" />
-                    <Text note style={styles.buttonLabel}> {global.switch_portalName}</Text>
+                    <Text note style={styles.buttonLabel}>
+                      {' '}
+                      {global.switch_portalName}
+                    </Text>
                   </View>
                 </Col>
               </Row>
-           
+
 
             </Grid>
           </View>
 
- 
+
           <View style={styles.newsContentLine}>
-
-
             <FlatList
               data={this.state.calendarEvents}
               keyExtractor={this.keyExtractor}
               renderItem={this._renderItem}
             />
-   {isAdmin(this.props.adminPassword) &&
+            {isAdmin(this.props.adminPassword)
+            && (
             <FlatList
               data={this.state.calendarEventsHidden}
               keyExtractor={this.keyExtractor}
               renderItem={this._renderItem}
             />
+            )
    }
 
           </View>
 
-          {instID == '0001-sais_edu_sg' &&
+          {instID == '0001-sais_edu_sg'
 
 
-            <TouchableOpacity style={{ flexDirection: 'row' }} onPress={() => { Actions.ptaHome(); }} >
+            && (
+            <TouchableOpacity style={{ flexDirection: 'row' }} onPress={() => { Actions.ptaHome(); }}>
 
               <View>
                 <View style={{ height: 60, backgroundColor: 'white', flexDirection: 'row' }}>
-                  <Image
-                    style={{ width: 36, height: 36, margin: 12, borderRadius: 18, borderWidth: StyleSheet.hairlineWidth, borderColor: 'lightgray' }}
-                    source={{ uri: `https://saispta.com/wp-content/uploads/2018/12/Screenshot-2018-12-10-15.49.39.png` }}
+      <Image
+                    style={{
+                      width: 36, height: 36, margin: 12, borderRadius: 18, borderWidth: StyleSheet.hairlineWidth, borderColor: 'lightgray',
+                    }}
+                    source={{ uri: 'https://saispta.com/wp-content/uploads/2018/12/Screenshot-2018-12-10-15.49.39.png' }}
                   />
-                  <Text style={{ fontWeight: 'bold', height: 60, lineHeight: 60, color: "black" }}>Parent Connections </Text>
+      <Text style={{
+                    fontWeight: 'bold', height: 60, lineHeight: 60, color: 'black',
+                  }}
+                  >
+Parent Connections
+                    {' '}
 
-                </View>
+                  </Text>
+
+    </View>
 
 
                 <View>
-                  <Image
-                    source={{ uri: `https://saispta.com/wp-content/uploads/2018/12/ISASAIS-2017-2018-0032-e1544427990824.jpg` }}
+      <Image
+                    source={{ uri: 'https://saispta.com/wp-content/uploads/2018/12/ISASAIS-2017-2018-0032-e1544427990824.jpg' }}
                     style={{ width, height: 200 }}
-                    resizeMode={'contain'}
+                    resizeMode="contain"
                   />
-                </View>
+    </View>
               </View>
             </TouchableOpacity>
+            )
 
 
-          }
-
-<View>
-                <View style={{ height: 60, backgroundColor: 'white', flexDirection: 'row' }}>
-                </View>
-                </View>
-
-
-          {instID == '0001-sais_edu_sg' &&
-                <Row style={{ paddingBottom: 20 }}>
-                             <Col>
-                  <Button transparent style={styles.roundedButton} onPress={() => { Actions.contact(); }} >
-                    <Ionicons name="ios-call" style={styles.icon} />
-                  </Button>
-                  <Text note style={styles.buttonLabel}>Contact</Text>
-                </Col>
-                 
-                  <Col>
-                    <Button transparent style={styles.roundedButton} onPress={() => { Actions.ptaHome(); }} >
-                      <Icon style={styles.icon} name="ios-people" />
-                    </Button>
-                    <Text note style={styles.buttonLabel}>PTA</Text>
-                  </Col>
-
-                  <Col>
-                    <Button transparent style={styles.roundedButton} onPress={() => { Actions.campusMap(); }} >
-                      <Icon style={styles.icon} name="ios-map" />
-                    </Button>
-                    <Text note style={styles.buttonLabel}>School Map</Text>
-                  </Col>
-                </Row>
-              }
-
-          {isAdmin(this.props.adminPassword) &&
-            <TouchableHighlight style={styles.addButton} underlayColor='#ff7043' onPress={() => Actions.storyForm(
-
-            )}>
-              <Text style={{ fontSize: 25, color: 'white' }}>+</Text>
-            </TouchableHighlight>
           }
 
           <View>
-            <Text style={styles.version}> </Text>
-            <Text style={styles.version}> </Text>
+            <View style={{ height: 60, backgroundColor: 'white', flexDirection: 'row' }} />
+          </View>
 
-            <Text style={styles.version}> </Text>
+
+          {instID == '0001-sais_edu_sg'
+                && (
+                <Row style={{ paddingBottom: 20 }}>
+                  <Col>
+                <Button transparent style={styles.roundedButton} onPress={() => { Actions.contact(); }}>
+                  <Ionicons name="ios-call" style={styles.icon} />
+                </Button>
+                <Text note style={styles.buttonLabel}>Contact</Text>
+              </Col>
+
+                  <Col>
+                <Button transparent style={styles.roundedButton} onPress={() => { Actions.ptaHome(); }}>
+                  <Icon style={styles.icon} name="ios-people" />
+                </Button>
+                <Text note style={styles.buttonLabel}>PTA</Text>
+              </Col>
+
+                  <Col>
+                <Button transparent style={styles.roundedButton} onPress={() => { Actions.campusMap(); }}>
+                  <Icon style={styles.icon} name="ios-map" />
+                </Button>
+                <Text note style={styles.buttonLabel}>School Map</Text>
+              </Col>
+                </Row>
+                )
+              }
+
+          {isAdmin(this.props.adminPassword)
+            && (
+            <TouchableHighlight
+              style={styles.addButton}
+              underlayColor="#ff7043"
+              onPress={() => Actions.storyForm(
+
+              )}
+            >
+              <Text style={{ fontSize: 25, color: 'white' }}>+</Text>
+            </TouchableHighlight>
+            )
+          }
+
+          <View>
+            <Text style={styles.version} />
+            <Text style={styles.version} />
+
+            <Text style={styles.version} />
           </View>
           <Image source={require('../../../images/sais.edu.sg/10yearLogo.png')} style={styles.tenYearLogo} />
 
@@ -307,11 +318,21 @@ class HomeNav extends Component {
           </TouchableOpacity>
 
           <View>
-            <Text style={styles.version}> </Text>
-            <Text style={styles.version}>{this.state.token} </Text>
-            <Text style={styles.version}>Version: {Constants.manifest.revisionId}</Text>
-            <Text style={styles.version}>{global.pushToken} </Text>
-            <Text style={styles.version}> </Text>
+            <Text style={styles.version} />
+            <Text style={styles.version}>
+              {this.state.token}
+              {' '}
+            </Text>
+            <Text style={styles.version}>
+              Version:
+              {' '}
+              {Constants.manifest.revisionId}
+            </Text>
+            <Text style={styles.version}>
+              {global.pushToken}
+              {' '}
+            </Text>
+            <Text style={styles.version} />
           </View>
 
 
@@ -327,16 +348,14 @@ function bindAction(dispatch) {
   };
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators(ActionCreators, dispatch);
-};
+const mapDispatchToProps = dispatch => bindActionCreators(ActionCreators, dispatch);
 
 const mapStateToProps = state => ({
   navigation: state.cardNavigation,
   userX: state.user,
   adminPassword: state.user.adminPassword,
   ffauth_device_idX: state.ffauth_device_id,
-  ffauth_secretX: state.ffauth_secret
+  ffauth_secretX: state.ffauth_secret,
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeNav);
