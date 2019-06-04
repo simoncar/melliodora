7e4c39adfd296bcf50c7c6f838de1817932d9def
Simoncar <simoncar@gmail.com>
2019-06-04 11:43:37 +0800

    Beacon screen display

--- js/components/beacon/index.js
+++ js/components/beacon/index.js
@@ -14,7 +14,6 @@ import { withMappedNavigationProps } from "react-navigation-props-mapper";
 
 import styles from "./styles";
 
-
 const BeaconItem = require("./beaconItem");
 let instID = Constants.manifest.extra.instance;
 
@@ -37,7 +36,9 @@ class beacons extends Component {
       userBeacons: {}
     };
 
-    this.beaconsFirebase = firebase.database().ref(`instance/${instID}/beacon`).orderByChild('timestamp');
+    this.beaconsFirebase = firebase
+      .database()
+      .ref(`instance/${instID}/beacon`)
     //this.beaconsFirebase = firebase.database().ref(`instance/${  instID  }/user/${ global.safeToken}/chatrooms`);
   }
 
@@ -54,9 +55,6 @@ class beacons extends Component {
   keyExtractor = item => item._key;
 
   listenLoadFromFirebase(beacons) {
-
-   
-
     beacons.on("value", dataSnapshot2 => {
       this.props.setUserBeacons(dataSnapshot2);
       dataSnapshot = dataSnapshot2;
@@ -65,30 +63,34 @@ class beacons extends Component {
       var beaconIcon = "";
 
       dataSnapshot.forEach(child => {
-      
-            ++this.countEntered;
-            beaconIcon = "G";
-         
-        
+        ++this.countEntered;
+        beaconIcon = "G";
 
-        this.state.userBeacons.push({
-          beaconCampus: child.val().beaconCampus,
-          beaconGrade: child.val().beaconGrade,
-          beaconIcon: beaconIcon,
-          beaconName: child.val().beaconName,
-          beaconType: child.val().beaconType,
-          beaconPictureURL: child.val().beaconPictureURL,
-          timestamp: child.val().timestamp,
-          lastSeen:  child.val().lastSeen,
-          _key: child.key
-
-          
-        });
-        console.log ("this.state.userBeacons.lastSeen=", child.val().lastSeen)
-          console.log ("this.state.userBeacons.timestamp=", child.val().timestamp)
+        if (
+          child.val().beaconName != "GATEWAY" &&
+          child.val().beaconName != ""
+        ) {
+          this.state.userBeacons.push({
+            beaconCampus: child.val().beaconCampus,
+            beaconGrade: child.val().beaconGrade,
+            beaconIcon: beaconIcon,
+            beaconName: child.val().beaconName,
+            beaconType: child.val().beaconType,
+            beaconPictureURL: child.val().beaconPictureURL,
+            timestamp: child.val().timestamp,
+            lastSeen: child.val().lastSeen,
+            state: child.val().state,
+            _key: child.key
+          });
+        }
       });
 
-      this.state.userBeacons.sort((a, b) => (a.timestamp > b.timestamp) ? 1 : (a.timestamp === undefined) ? ((a.lastSeen < b.lastSeen) ? 1 : -1) : -1 )
+      //this.state.userBeacons.sort((a, b) => (a.timestamp > b.timestamp) ? 1 : (a.timestamp === undefined) ? ((a.lastSeen < b.lastSeen) ? 1 : -1) : -1 )
+      this.state.userBeacons.sort((a, b) => {
+        if (a.state < b.state) return -1;
+        if (a.state > b.state) return 1;
+        return 0;
+      });
 
       this.setState({
         beacons
@@ -113,74 +115,59 @@ class beacons extends Component {
     );
   }
 
-
   renderCount(item, count) {
-    
-      return (
-        <View
-          style={{
-            borderRadius: 30,
-            backgroundColor: "#1DAEF2",
-            width: 45,
-            height: 45,
-            marginLeft: 10,
-            marginTop: 10,
-            alignItems: "center",
-            paddingLeft: 0,
-            paddingRight: 0,
-            justifyContent: "center"
-          }}
-        >
-           <View>
-            <Text>{count}</Text>
-          </View>
-
+    return (
+      <View
+        style={{
+          borderRadius: 30,
+          backgroundColor: "#1DAEF2",
+          width: 45,
+          height: 45,
+          marginLeft: 10,
+          marginTop: 10,
+          alignItems: "center",
+          paddingLeft: 0,
+          paddingRight: 0,
+          justifyContent: "center"
+        }}
+      >
+        <View>
+          <Text>{count}</Text>
         </View>
-        
-      );
-    
+      </View>
+    );
   }
 
   render() {
     return (
       <Container style={styles.container}>
-        
-  
         <View style={{ height: 100 }}>
-        <Grid style={{ height: 50 }}>
-          <Col style={{ alignItems: 'center' }}>
-            <Row>
-            {this.renderCount("Total",3028)}
-            </Row>
-            <Row>
-              <Text style={styles.chatTitle}>Total</Text>
-            </Row>
-          </Col>
-          <Col style={{ alignItems: 'center' }}>
-            <Row>
-            {this.renderCount("Entered",this.countEntered)}
-            </Row>
-            <Row>
-              <Text style={styles.chatTitle}>Entered</Text>
-            </Row>
-          </Col>
-          <Col style={{ alignItems: 'center' }}>
-            <Row>
-            {this.renderCount("Exited", 0)}
-            </Row>
-            <Row>
-              <Text style={styles.chatTitle}>Exited</Text>
-            </Row>
-          </Col>
-          <Col style={{ alignItems: 'center' }}>
-            <Row>
-            {this.renderCount("No Show",3028 - this.countEntered)}
-            </Row>
-            <Row>
-              <Text style={styles.chatTitle}>No Show</Text>
-            </Row>
-          </Col>
-        </Grid>
+          <Grid style={{ height: 50 }}>
+            <Col style={{ alignItems: "center" }}>
+              <Row>{this.renderCount("Total", 3028)}</Row>
+              <Row>
+                <Text style={styles.chatTitle}>Total</Text>
+              </Row>
+            </Col>
+            <Col style={{ alignItems: "center" }}>
+              <Row>{this.renderCount("Entered", this.countEntered)}</Row>
+              <Row>
+                <Text style={styles.chatTitle}>Entered</Text>
+              </Row>
+            </Col>
+            <Col style={{ alignItems: "center" }}>
+              <Row>{this.renderCount("Exited", 0)}</Row>
+              <Row>
+                <Text style={styles.chatTitle}>Exited</Text>
+              </Row>
+            </Col>
+            <Col style={{ alignItems: "center" }}>
+              <Row>{this.renderCount("No Show", 3028 - this.countEntered)}</Row>
+              <Row>
+                <Text style={styles.chatTitle}>No Show</Text>
+              </Row>
+            </Col>
+          </Grid>
         </View>
 
         <Content style={{ paddingTop: 5 }}>
