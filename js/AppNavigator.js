d70c3ffa86ecc39749b4629db2c743951f843aa2
Simoncar <simoncar@gmail.com>
2019-06-22 13:37:39 +0800

    Cleanup

--- js/AppNavigator.js
+++ js/AppNavigator.js
@@ -1,25 +1,18 @@
-import React, { Component } from "react";
-import { StatusBar, StyleSheet } from "react-native";
-
+import React from "react";
+import { StyleSheet } from "react-native";
 
 import {
   createAppContainer,
   createStackNavigator,
-  Header,
-  NavigationActions,
-  HeaderBackButton
 } from "react-navigation";
 
 import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
-import { MaterialIcons } from "@expo/vector-icons";
 import Login from "./components/login/";
 import Home from "./components/home/";
 import phoneCalendar from "./components/home/calendars";
 import HomeNav from "./components/homeNav/";
 import Contact from "./components/contact";
-
 import Settings from "./components/settings";
-
 import Story from "./components/story";
 import StoryForm from "./components/story/form";
 import campusMap from "./components/campusMap";
@@ -30,18 +23,8 @@ import chatmain from "./components/chat/main";
 import Webportal from "./components/webportal";
 import WebportalAuth from "./components/webportal/auth";
 import WebportalSports from "./components/webportalSports";
-import CustomHeader from "./CustomHeader";
 import beacon from "./components/beacon";
 import beaconHistory from "./components/beacon/beaconHistory";
-
-const tabBarIcon = name => ({ tintColor }) => (
-  <MaterialIcons
-    style={{ backgroundColor: "transparent" }}
-    name={name}
-    color={tintColor}
-    size={24}
-  />
-);
 
 let Tabs = createMaterialBottomTabNavigator(
   {
@@ -102,14 +85,3 @@ const MainScreenNavigator = createStackNavigator({
 });
 
 export default createAppContainer(MainScreenNavigator);
-
-let styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  paragraph: {
-    fontSize: 18
-  }
-});
