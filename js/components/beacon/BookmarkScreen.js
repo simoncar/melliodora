9579d0518e658456e74cda10bb52faae624d1db3
Wei Shen <weishenbs@gmail.com>
2019-07-06 15:24:37 +0800

    update bookmark screen

--- js/components/beacon/BookmarkScreen.js
+++ js/components/beacon/BookmarkScreen.js
@@ -9,11 +9,57 @@ import BookmarkHooks from "./hooks/BookmarkHook2";
 
 const BookmarkScreen = () => {
 
-  const { addBookmark, removeBookmark, retrieveBookmarkData, checkBookmarked, bookmarksData } = BookmarkHooks();
+  const { addBookmark, removeBookmark, bookmarksData, bookmarks } = BookmarkHooks();
 
+
+  _renderItem = (item, index) => {
+    const avatar = item.imgSrc ? { source: { uri: item.imgSrc } } : { title: 'MD' };
+
+    let onPressFunc, color;
+
+    if (bookmarks.indexOf(item.mac) > -1) {
+      onPressFunc = removeBookmark;
+      color = "gold";
+    } else {
+      onPressFunc = addBookmark;
+      color = "gray";
+    }
+
+    return (
+      <ListItem
+        leftAvatar={{ rounded: true, ...avatar }}
+        title={
+          <View style={{ flex: 1, flexDirection: 'row' }}>
+            <Text style={{ flex: 1, fontSize: 14 }}>{item.mac}</Text>
+            <Text style={{ flex: 0, flexShrink: 1, fontSize: 8, alignSelf: 'center', justifyContent: 'center', color: 'gray' }}>{item.mac}</Text>
+          </View>
+        }
+        chevron={false}
+        subtitle={
+          <View style={{ flex: 1, flexDirection: 'column', paddingTop: 8}}>
+            <Text style={{ color: 'gray', fontSize: 12 }}>Class {item.campus}</Text>
+            <Text style={{ color: 'gray', fontSize: 12 }}>last seen {moment(item.lastSeen).format("LLL")}</Text>
+            <Text style={{ color: 'gray', fontSize: 12 }}>current status {item.state}</Text>
+          </View>
+        }
+
+        rightIcon={
+          <TouchableOpacity style={{ paddingRight: 8 }} onPress={() => onPressFunc(item.mac)}>
+            <FontAwesome name="star" size={28} color={color} />
+          </TouchableOpacity>
+
+        }
+        onPress={() => this.props.navigation.navigate("AttendeeDetailScreen", item)}
+        topDivider={true}
+        containerStyle={{margin: 10}}
+      />
+    );
+  };
+
+  console.log("bookmarks",bookmarks);
   console.log("bookmarksData",bookmarksData);
   return(
-    <Text>{JSON.stringify(bookmarksData)}</Text>
+    <View>{bookmarksData.map(this._renderItem)}</View>
   );
 }
 
