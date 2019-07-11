350a040cd3427efad6456e3fb1a50c29b1152597
Wei Shen <weishenbs@gmail.com>
2019-07-11 14:25:39 +0800

    optimize bookmarks

--- js/components/beacon/utils/BookmarkStore.js
+++ js/components/beacon/utils/BookmarkStore.js
@@ -17,7 +17,7 @@ const init = async store => {
   if (store.state.initial == false) return;
   console.log("init2");
   const bookmarks = await retrieveBookmarks();
-  const bookmarksData = await getData(bookmarks);
+  const bookmarksData = await getData(bookmarks.filter(b => typeof b === 'string'));
   // const updatedBookmarksData = bookmarksData.filter(b => { if (b) return b });
   // const updatedBookmarks = updatedBookmarksData.filter(b => b.mac);
 
@@ -32,6 +32,35 @@ const init = async store => {
 
   AsyncStorage.setItem('myBookmarks', JSON.stringify(bk))
   store.setState({ initial: false, bookmarks: bk, bookmarksData: bkData, loading: false });
+}
+
+const _removeBookamarksWithMac = (mac, bookmarksData) => {
+  return { updatedBookmarks, updatedBookmarksData } = bookmarksData.reduce((a,b, i) => {
+    if (b.mac !== mac) {
+      a.updatedBookmarks.push(b.mac);
+      a.updatedBookmarksData.push(b);
+    }
+    return a;
+
+  }, { updatedBookmarks: [], updatedBookmarksData: [] });
+}
+
+const addBookmarkWithInfo = (store, bookmarkInfo, insertIndex = null) => {
+  console.log("hit add 2");
+
+  store.setState({ loading: true });
+  const bookmarksData = store.state.bookmarksData;
+  const { updatedBookmarks, updatedBookmarksData } = _removeBookamarksWithMac(bookmarkInfo.mac, bookmarksData);
+  if(typeof insertIndex == 'number') {
+    updatedBookmarks.splice(insertIndex, 0, bookmarkInfo.mac);
+    updatedBookmarksData.splice(insertIndex, 0, bookmarkInfo);
+  } else {
+    updatedBookmarks.push(bookmarkInfo.mac);
+    updatedBookmarksData.push(bookmarkInfo);
+  }
+
+  AsyncStorage.setItem('myBookmarks', JSON.stringify(updatedBookmarks))
+    .then(() => store.setState({ bookmarks: updatedBookmarks, bookmarksData: updatedBookmarksData, loading: false }));
 }
 
 const addBookmark = (store, mac) => {
@@ -49,12 +78,8 @@ const addBookmark = (store, mac) => {
 const removeBookmark = (store, mac) => {
   console.log("hit remove");
   store.setState({ loading: true });
-  const bookmarks = store.state.bookmarks;
-  const updatedBookmarks = bookmarks.filter(e => e !== mac);
-
   const bookmarksData = store.state.bookmarksData;
-  const updatedBookmarksData = bookmarksData.filter(e => e.mac !== mac);
-
+  const { updatedBookmarks, updatedBookmarksData } = _removeBookamarksWithMac(mac, bookmarksData);
   AsyncStorage.setItem('myBookmarks', JSON.stringify(updatedBookmarks))
     .then(() => store.setState({ bookmarks: updatedBookmarks, bookmarksData: updatedBookmarksData, loading: false }));
 }
@@ -97,7 +122,7 @@ const getData = (bm) => {
     });
 }
 
-const actions = { init, addBookmark, removeBookmark };
+const actions = { init, addBookmark, removeBookmark, addBookmarkWithInfo };
 const useGlobal = useGlobalHook(React, initialState, actions);
 
 export default useGlobal;
