a2c79e4a20bc5a06a94d2c605feb0a2930bfc171
Simoncar <simoncar@gmail.com>
2019-07-12 08:52:28 -0700

    change export class

--- js/lib/database.js
+++ js/lib/database.js
@@ -1,50 +1,46 @@
-
-
 /**
  * @class Database
  */
 
 import * as firebase from "firebase";
 
-class Database {
+export default class Database {
+  /**
+   * Sets a users mobile number
+   * @param userId
+   * @param mobile
+   * @returns {firebase.Promise<any>|!firebase.Promise.<void>}
+   */
+  static setUserMobile(userId, mobile) {
+    let userMobilePath = "/user/" + userId + "/details";
 
-    /**
-     * Sets a users mobile number
-     * @param userId
-     * @param mobile
-     * @returns {firebase.Promise<any>|!firebase.Promise.<void>}
-     */
-    static setUserMobile(userId, mobile) {
+    return firebase
+      .database()
+      .ref(userMobilePath)
+      .set({
+        mobile: mobile,
+      });
+  }
 
-        let userMobilePath = "/user/" + userId + "/details";
+  /**
+   * Listen for changes to a users mobile number
+   * @param userId
+   * @param callback Users mobile number
+   */
+  static listenUserMobile(userId, callback) {
+    let userMobilePath = "/user/" + userId + "/details";
 
-        return firebase.database().ref(userMobilePath).set({
-            mobile: mobile
-        })
+    firebase
+      .database()
+      .ref(userMobilePath)
+      .on("value", snapshot => {
+        var mobile = "";
 
-    }
+        if (snapshot.val()) {
+          mobile = snapshot.val().mobile;
+        }
 
-    /**
-     * Listen for changes to a users mobile number
-     * @param userId
-     * @param callback Users mobile number
-     */
-    static listenUserMobile(userId, callback) {
-
-        let userMobilePath = "/user/" + userId + "/details";
-
-        firebase.database().ref(userMobilePath).on('value', (snapshot) => {
-
-            var mobile = "";
-
-            if (snapshot.val()) {
-                mobile = snapshot.val().mobile
-            }
-
-            callback(mobile)
-        });
-    }
-
+        callback(mobile);
+      });
+  }
 }
-
-module.exports = Database;
