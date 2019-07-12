e41f4eccf5808bd11365ef080b8b96aacc118137
Wei Shen <weishenbs@gmail.com>
2019-07-12 12:44:40 +0800

    completed basic drill down feature

--- js/components/beacon/ClassListingScreen.js
+++ js/components/beacon/ClassListingScreen.js
@@ -1,26 +1,59 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, FlatList } from "react-native";
+import { StyleSheet, View, FlatList, ActivityIndicator } from "react-native";
 import { ListItem, SearchBar, Text } from "react-native-elements";
+import firebase from "firebase";
 
 import useBeaconSearchHook from "./utils/BeaconSearchStore";
 
 const ClassListingScreen = ({ navigation }) => {
 
-  const [globalBeaconSearch, globalBeaconSearchAction] = useBeaconSearchHook();
+  const [globalBeaconSearchState, globalBeaconSearchAction] = useBeaconSearchHook();
   const [loading, setLoading] = useState(false);
-  const [classData, setClassData] = useState([
-    { classID: "2DAYE", amount: 20 },
-    { classID: "2GHRD", amount: 18 },
-    { classID: "2RFSU", amount: 22 },
-    { classID: "2KLUG", amount: 23 },
-    { classID: "2SSSS", amount: 22 }
-  ]);
+  const [classData, setClassData] = useState([]);
+
+  const { grade } = globalBeaconSearchState;
+  console.log("type of grade", typeof grade);
+
+  useEffect(() => {
+    retrieveData();
+  }, []);
+
+  // Retrieve Data
+  retrieveData = async () => {
+    try {
+
+      // Set State: Loading
+      setLoading(true);
+      console.log('Retrieving Data');
+      // Cloud Firestore: Query
+
+      let initialQuery = await firebase
+        .firestore()
+        .collection("sais_edu_sg")
+        .doc("org")
+        .collection("class")
+        .where("grade", "==", grade);
+
+
+      // Cloud Firestore: Query Snapshot
+      let documentSnapshots = await initialQuery.get();
+      // Cloud Firestore: Document Data
+      let newDocumentData = documentSnapshots.docs.map(document => document.data());
+
+      // Set State
+      setClassData(newDocumentData);
+      setLoading(false);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
 
   _keyExtractor = (item, index) => index.toString();
 
   _renderItem = ({ item }) => (
     <ListItem
-      title={item.classID}
+      title={item.classCode}
       chevron={true}
       rightIcon={{ name: "person" }}
       // badge={{
@@ -35,11 +68,8 @@ const ClassListingScreen = ({ navigation }) => {
       //   }
       // }}
       onPress={() => {
-        globalBeaconSearchAction.setClass(item.classID);
-        navigation.navigate("AttendeeListingScreen", {
-          title: navigation.getParam("title"),
-          selectedGrade: item.classID
-        });
+        globalBeaconSearchAction.setClass(item.classCode);
+        navigation.navigate("AttendeeListingScreen");
       }}
     />
   );
@@ -56,6 +86,26 @@ const ClassListingScreen = ({ navigation }) => {
     );
   };
 
+  // Render Footer
+  renderFooter = () => {
+    try {
+
+      // Check If Loading
+
+      if (loading) {
+        return (
+          <ActivityIndicator />
+        )
+      }
+      else {
+        return null;
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
 
   return (
     <View>
@@ -65,6 +115,7 @@ const ClassListingScreen = ({ navigation }) => {
         renderItem={_renderItem}
         keyExtractor={_keyExtractor}
         ItemSeparatorComponent={renderSeparator}
+        ListFooterComponent={renderFooter}
       />
     </View>
   );
