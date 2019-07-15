c7ce32be8a11e8f9319a59f9585c5ca4df74f8a3
Simoncar <simoncar@gmail.com>
2019-07-15 14:26:01 -0400

    Flex to list so we can see the last item

--- js/components/beacon/AttendeeListingScreen.js
+++ js/components/beacon/AttendeeListingScreen.js
@@ -1,7 +1,7 @@
 // Imports: Dependencies
-import React, { useState, useEffect } from 'react';
-import { ActivityIndicator, Dimensions, FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import React, { useState, useEffect } from "react";
+import { ActivityIndicator, Dimensions, FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { ListItem } from "react-native-elements";
 import moment from "moment";
 
 import firebase from "firebase";
@@ -9,11 +9,10 @@ import firebase from "firebase";
 import useBeaconSearchHook from "./utils/BeaconSearchStore";
 
 // Screen Dimensions
-const { height, width } = Dimensions.get('window');
+const { height, width } = Dimensions.get("window");
 // Screen: Infinite Scroll
 
 const AttendeeListingScreen = ({ navigation }) => {
-
   const [globalBeaconSearchState, globalBeaconSearchAction] = useBeaconSearchHook();
   const { beaconState, grade } = globalBeaconSearchState;
   const studentClass = globalBeaconSearchState.class;
@@ -25,21 +24,20 @@ const AttendeeListingScreen = ({ navigation }) => {
   const [loading, setLoading] = useState(false);
   const [refreshing, setRefreshing] = useState(false);
 
-
   useEffect(() => {
     retrieveData();
   }, []);
 
   getQuery = () => {
-    if(!beaconState){
+    if (!beaconState) {
       return firebase
-      .firestore()
-      .collection("sais_edu_sg")
-      .doc("beacon")
-      .collection("beacons")
-      .where("grade", "==", String(grade))
-      .where("class", "==", studentClass)
-      .orderBy('mac');
+        .firestore()
+        .collection("sais_edu_sg")
+        .doc("beacon")
+        .collection("beacons")
+        .where("grade", "==", String(grade))
+        .where("class", "==", studentClass)
+        .orderBy("mac");
     }
     return firebase
       .firestore()
@@ -49,19 +47,17 @@ const AttendeeListingScreen = ({ navigation }) => {
       .where("state", "==", beaconState)
       .where("grade", "==", String(grade))
       .where("class", "==", studentClass)
-      .orderBy('mac');
-  }
+      .orderBy("mac");
+  };
   // Retrieve Data
   retrieveData = async () => {
     try {
-
       // Set State: Loading
       setLoading(true);
-      console.log('Retrieving Data');
+      console.log("Retrieving Data");
       // Cloud Firestore: Query
 
-      let initialQuery = await getQuery()
-        .limit(30);
+      let initialQuery = await getQuery().limit(30);
 
       // Cloud Firestore: Query Snapshot
       let documentSnapshots = await initialQuery.get();
@@ -81,24 +77,21 @@ const AttendeeListingScreen = ({ navigation }) => {
         setDocumentData([]);
         setLoading(false);
       }
-
-    }
-    catch (error) {
+    } catch (error) {
       console.log(error);
     }
   };
   // Retrieve More
   retrieveMore = async () => {
     try {
-
       if (!documentData) return;
       // Set State: Refreshing
       setRefreshing(true);
-      console.log('Retrieving additional Data');
+      console.log("Retrieving additional Data");
       // Cloud Firestore: Query (Additional Query)
       let additionalQuery = await getQuery()
         .startAfter(lastVisible)
-        .limit(limit)
+        .limit(limit);
       // Cloud Firestore: Query Snapshot
       let documentSnapshots = await additionalQuery.get();
       // Cloud Firestore: Document Data
@@ -109,38 +102,29 @@ const AttendeeListingScreen = ({ navigation }) => {
       setDocumentData([...documentData, ...newDocumentData]);
       setLastVisible(newlastVisible);
       setRefreshing(false);
-    }
-    catch (error) {
+    } catch (error) {
       console.log(error);
     }
   };
   // Render Header
   renderHeader = () => {
     try {
-      return (
-        null
-      )
-    }
-    catch (error) {
+      return null;
+    } catch (error) {
       console.log(error);
     }
   };
   // Render Footer
   renderFooter = () => {
     try {
-
       // Check If Loading
 
       if (loading) {
-        return (
-          <ActivityIndicator />
-        )
-      }
-      else {
+        return <ActivityIndicator />;
+      } else {
         return null;
       }
-    }
-    catch (error) {
+    } catch (error) {
       console.log(error);
     }
   };
@@ -154,17 +138,28 @@ const AttendeeListingScreen = ({ navigation }) => {
       <ListItem
         leftAvatar={{ rounded: true, ...avatar }}
         title={
-          <View style={{ flex: 1, flexDirection: 'row' }}>
+          <View style={{ flex: 1, flexDirection: "row" }}>
             <Text style={{ flex: 1, fontSize: 16 }}>{item.fullname || "No Name"}</Text>
-            <Text style={{ flex: 0, flexShrink: 1, fontSize: 10, alignSelf: 'center', justifyContent: 'center', color: 'gray' }}>{item.mac}</Text>
+            <Text
+              style={{
+                flex: 0,
+                flexShrink: 1,
+                fontSize: 10,
+                alignSelf: "center",
+                justifyContent: "center",
+                color: "gray",
+              }}
+            >
+              {item.mac}
+            </Text>
           </View>
         }
         chevron={true}
         subtitle={
-          <View style={{ flex: 1, flexDirection: 'column', paddingTop: 8 }}>
-            <Text style={{ color: 'gray' }}>Class {item.class}</Text>
-            <Text style={{ color: 'gray' }}>last seen {moment(item.lastSeen).format("LLL")}</Text>
-            <Text style={{ color: 'gray' }}>{item.state}</Text>
+          <View style={{ flex: 1, flexDirection: "column", paddingTop: 8 }}>
+            <Text style={{ color: "gray" }}>Class {item.class}</Text>
+            <Text style={{ color: "gray" }}>last seen {moment(item.lastSeen).format("LLL")}</Text>
+            <Text style={{ color: "gray" }}>{item.state}</Text>
           </View>
         }
         onPress={() => navigation.navigate("AttendeeDetailScreen", item)}
@@ -178,12 +173,11 @@ const AttendeeListingScreen = ({ navigation }) => {
         style={{
           height: 1,
           width: "100%",
-          backgroundColor: "#CED0CE"
+          backgroundColor: "#CED0CE",
         }}
       />
     );
   };
-
 
   return (
     <SafeAreaView style={styles.container}>
@@ -191,72 +185,64 @@ const AttendeeListingScreen = ({ navigation }) => {
         // Data
 
         data={documentData}
-
         // Render Items
 
         renderItem={_renderItem}
-
         ItemSeparatorComponent={renderSeparator}
-
         // Item Key
 
         keyExtractor={(item, index) => String(index)}
-
         // Header (Title)
 
         ListHeaderComponent={renderHeader}
-
         // Footer (Activity Indicator)
 
         ListFooterComponent={renderFooter}
-
         // On End Reached (Takes a function)
 
         onEndReached={retrieveMore}
-
         // How Close To The End Of List Until Next Data Request Is Made
 
         onEndReachedThreshold={0.1}
-
         // Refreshing (Set To True When End Reached)
         refreshing={refreshing}
       />
     </SafeAreaView>
-  )
-
-}
+  );
+};
 // Styles
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     height: height,
     width: width,
   },
   headerText: {
-    fontFamily: 'System',
+    fontFamily: "System",
     fontSize: 36,
-    fontWeight: '600',
-    color: '#000',
+    fontWeight: "600",
+    color: "#000",
     marginLeft: 12,
     marginBottom: 12,
   },
   itemContainer: {
     height: 80,
     width: width,
-    borderWidth: .2,
-    borderColor: '#000',
-    justifyContent: 'center',
-    alignItems: 'center',
+    borderWidth: 0.2,
+    borderColor: "#000",
+    justifyContent: "center",
+    alignItems: "center",
   },
   text: {
-    fontFamily: 'System',
+    fontFamily: "System",
     fontSize: 16,
-    fontWeight: '400',
-    color: '#000',
+    fontWeight: "400",
+    color: "#000",
   },
 });
 
 AttendeeListingScreen.navigationOptions = {
   title: "Select Student",
-  headerBackTitle: null
+  headerBackTitle: null,
 };
 export default AttendeeListingScreen;
