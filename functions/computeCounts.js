81c75a885f1fac94edb309a4755d4637ed471cf5
Simoncar <simoncar@gmail.com>
2019-07-18 10:10:43 -0700

    compute Daily Counts

---
+++ functions/computeCounts.js
@@ -0,0 +1,8 @@
+const firebase = require("firebase-admin");
+const moment = require("moment");
+
+//export GOOGLE_APPLICATION_CREDENTIALS="/Users/simon/Documents/code/app/functions/Calendar App-915d5dbe4185.json"
+
+const computeCounts = () => {};
+
+module.exports = computeCounts;
