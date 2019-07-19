efc3e2090b5959bbf4a463504ce119a2135507c3
Simoncar <simoncar@gmail.com>
2019-07-19 08:25:49 -0700

    version updates

--- js/components/beacon/BeaconSearch.js
+++ js/components/beacon/BeaconSearch.js
@@ -1,42 +1,59 @@
-import React, { Component } from 'react'
-import { Text, StyleSheet, View, TouchableOpacity, Picke, TextInput, ActivityIndicator, Dimensions, FlatList, SafeAreaView } from 'react-native'
+import React, { Component } from "react";
+import {
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Picke,
+  TextInput,
+  ActivityIndicator,
+  Dimensions,
+  FlatList,
+  SafeAreaView,
+} from "react-native";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
-import { ButtonGroup, Input, Button } from 'react-native-elements';
+import { ButtonGroup, Input, Button } from "react-native-elements";
 import firebase from "firebase";
-import { ListItem } from 'react-native-elements';
+import { ListItem } from "react-native-elements";
 import moment from "moment";
 
 export default class BeaconSearch extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       documentData: [],
       loading: false,
-      searchTerm: '',
+      searchTerm: "",
       selectedIndex: 0,
-      noResult: false
+      noResult: false,
     };
   }
 
-  updateIndex = (selectedIndex) => {
-    this.setState({ selectedIndex })
-  }
+  updateIndex = selectedIndex => {
+    this.setState({ selectedIndex });
+  };
 
   getQuery = () => {
+    // baseQuery = firebase
+    //   .firestore()
+    //   .collection("sais_edu_sg")
+    //   .doc("beacon")
+    //   .collection("beacons")
+    // if (this.state.selectedIndex === 0) {
+    //   return baseQuery
+    //     .where("mac", "==", this.state.searchTerm.toUpperCase())
+    // }
+    // return baseQuery
+    //   .where("fullname", "==", this.state.searchTerm)
 
     baseQuery = firebase
       .firestore()
       .collection("sais_edu_sg")
       .doc("beacon")
-      .collection("beacons")
-    if (this.state.selectedIndex === 0) {
-      return baseQuery
-        .where("mac", "==", this.state.searchTerm.toUpperCase())
-    }
-    return baseQuery
-      .where("fullname", "==", this.state.searchTerm)
-  }
+      .collection("beacons");
+
+    return baseQuery.where("searchArray", "array-contains", this.state.searchTerm.toUpperCase());
+  };
   // Retrieve Data
   retrieveData = async () => {
     try {
@@ -44,29 +61,27 @@ export default class BeaconSearch extends Component {
       this.setState({
         loading: true,
       });
-      console.log('Retrieving Data');      // Cloud Firestore: Query
-      let initialQuery = await this.getQuery()
+      console.log("Retrieving Data"); // Cloud Firestore: Query
+      let initialQuery = await this.getQuery();
 
       // Cloud Firestore: Query Snapshot
-      let documentSnapshots = await initialQuery.get();      // Cloud Firestore: Document Data
+      let documentSnapshots = await initialQuery.get(); // Cloud Firestore: Document Data
       let documentData = documentSnapshots.docs.map(document => document.data());
       if (documentData.length > 0) {
         this.setState({
           documentData: documentData,
           loading: false,
-          noResult: false
+          noResult: false,
         });
       } else {
         //No Result found
         this.setState({
           documentData: documentData,
           loading: false,
-          noResult: true
+          noResult: true,
         });
       }
-
-    }
-    catch (error) {
+    } catch (error) {
       console.log(error);
     }
   };
@@ -74,45 +89,38 @@ export default class BeaconSearch extends Component {
   // Render Header
   renderHeader = () => {
     try {
-      return (
-        null
-      )
-    }
-    catch (error) {
+      return null;
+    } catch (error) {
       console.log(error);
     }
   };
   // Render Footer
   renderFooter = () => {
     try {
-
       // Check If Loading
 
       if (this.state.loading) {
-        return (
-          <ActivityIndicator />
-        )
-      }
-      else {
+        return <ActivityIndicator />;
+      } else {
         return null;
       }
-    }
-    catch (error) {
+    } catch (error) {
       console.log(error);
     }
   };
 
   ListEmpty = () => {
-
     if (this.state.noResult) {
       return (
         //View to show when list is empty
-        <View style={{
-          justifyContent: 'center',
-          flex: 1,
-          margin: 10,
-        }}>
-          <Text style={{ color: 'gray', textAlign: 'center' }}>No Result Found</Text>
+        <View
+          style={{
+            justifyContent: "center",
+            flex: 1,
+            margin: 10,
+          }}
+        >
+          <Text style={{ color: "gray", textAlign: "center" }}>No Result Found</Text>
         </View>
       );
     }
@@ -129,17 +137,28 @@ export default class BeaconSearch extends Component {
       <ListItem
         leftAvatar={{ rounded: true, ...avatar }}
         title={
-          <View style={{ flex: 1, flexDirection: 'row' }}>
+          <View style={{ flex: 1, flexDirection: "row" }}>
             <Text style={{ flex: 1, fontSize: 16 }}>{item.fullname || "No Name"}</Text>
-            <Text style={{ flex: 0, flexShrink: 1, fontSize: 10, alignSelf: 'center', justifyContent: 'center', color: 'gray' }}>{item.mac}</Text>
+            <Text
+              style={{
+                flex: 0,
+                flexShrink: 1,
+                fontSize: 10,
+                alignSelf: "center",
+                justifyContent: "center",
+                color: "gray",
+              }}
+            >
+              {item.mac}
+            </Text>
           </View>
         }
         chevron={true}
         subtitle={
-          <View style={{ flex: 1, flexDirection: 'column', paddingTop: 8 }}>
-            <Text style={{ color: 'gray' }}>Class {item.class}</Text>
-            <Text style={{ color: 'gray' }}>last seen {moment(item.lastSeen).format("LLL")}</Text>
-            <Text style={{ color: 'gray' }}>{item.state}</Text>
+          <View style={{ flex: 1, flexDirection: "column", paddingTop: 8 }}>
+            <Text style={{ color: "gray" }}>Class {item.class}</Text>
+            <Text style={{ color: "gray" }}>last seen {moment(item.lastSeen).format("LLL")}</Text>
+            <Text style={{ color: "gray" }}>{item.state}</Text>
           </View>
         }
         onPress={() => this.props.navigation.navigate("AttendeeDetailScreen", item)}
@@ -153,56 +172,39 @@ export default class BeaconSearch extends Component {
         style={{
           height: 1,
           width: "100%",
-          backgroundColor: "#CED0CE"
+          backgroundColor: "#CED0CE",
         }}
       />
     );
   };
 
   render() {
-    const buttons = ['Beacon ID/MAC', 'Student Name'];
+    const buttons = ["Beacon ID/MAC", "Student Name"];
     const { selectedIndex } = this.state;
 
     return (
       <View style={{ padding: 10 }}>
         <Input
-          placeholder='Search'
-          leftIcon={
-            <Ionicons
-              name='ios-search'
-              size={24}
-              style={{ paddingRight: 12 }}
-              color='gray'
-            />
-          }
+          placeholder="Search"
+          leftIcon={<Ionicons name="ios-search" size={24} style={{ paddingRight: 12 }} color="gray" />}
           shake={true}
           leftIconContainerStyle={{ marginLeft: 0, paddingLeft: 0 }}
-          containerStyle={{ borderWidth: 1, borderRadius: 25, borderColor: 'gray' }}
+          containerStyle={{ borderWidth: 1, borderRadius: 25, borderColor: "gray" }}
           inputContainerStyle={{ borderBottomWidth: 0 }}
-          onChangeText={(text) => this.setState({ searchTerm: text })}
+          onChangeText={text => this.setState({ searchTerm: text })}
           value={this.state.searchTerm}
         />
-        {(this.state.selectedIndex === 1) &&
-          <Text style={{ color: 'red', fontSize: 8, marginLeft: 20 }}>* Case Sensitive</Text>
-        }
-        <ButtonGroup
-          onPress={this.updateIndex}
-          selectedIndex={selectedIndex}
-          buttons={buttons}
-        />
-        <Button
-          title="Submit"
-          raised
-          onPress={this.retrieveData}
-        />
-
+        {this.state.selectedIndex === 1 && (
+          <Text style={{ color: "red", fontSize: 8, marginLeft: 20 }}>* Case Sensitive</Text>
+        )}
+        <ButtonGroup onPress={this.updateIndex} selectedIndex={selectedIndex} buttons={buttons} />
+        <Button title="Submit" raised onPress={this.retrieveData} />
 
         <FlatList
           // Data
           data={this.state.documentData}
           // Render Items
           renderItem={this._renderItem}
-
           ItemSeparatorComponent={this.renderSeparator}
           // Item Key
           keyExtractor={(item, index) => String(index)}
@@ -212,12 +214,9 @@ export default class BeaconSearch extends Component {
           ListFooterComponent={this.renderFooter}
           ListEmptyComponent={this.ListEmpty}
         />
-
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
