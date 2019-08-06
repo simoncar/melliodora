9c07c8ca0cdb2cf60802014732fe4f2f2ad58c29
Simoncar <simoncar@gmail.com>
2019-08-06 09:45:11 +0800

    Action Sheet Button

--- js/components/webportalURL/styles.js
+++ js/components/webportalURL/styles.js
@@ -12,24 +12,32 @@ export default {
     paddingTop: 10,
     paddingLeft: 10,
     paddingRight: 10,
-    backgroundColor: "blue"
+    backgroundColor: "blue",
   },
   settingsMessageText: {
     fontSize: 22,
     paddingBottom: 10,
-    color: "white"
+    color: "white",
   },
   settingsMessageIcon: {
     fontSize: 30,
     width: 30,
-    color: "white"
+    color: "white",
   },
 
   navIcon: {
     paddingLeft: 10,
     paddingRight: 10,
     color: "#FFF",
-    fontSize: 20
+    fontSize: 20,
+  },
+
+  Heading: {
+    color: "#037AFF",
+    alignSelf: "center",
+    fontSize: 30,
+    paddingBottom: 5,
+    paddingRight: 10,
   },
 
   roundedButton: {
@@ -38,26 +46,26 @@ export default {
     backgroundColor: "rgba(0,0,0,0.2)",
     borderRadius: 30,
     width: 60,
-    height: 60
+    height: 60,
   },
   url: {
     color: "#FFF",
     width: 250,
-    fontSize: 14
+    fontSize: 14,
   },
   container: {
     flex: 1,
     paddingTop: 15 /* Padding to push below the navigation bar */,
-    backgroundColor: "#F5FCFF"
+    backgroundColor: "#F5FCFF",
   },
   topbar: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
     height: 40,
-    backgroundColor: "grey"
+    backgroundColor: "grey",
   },
   topbarTextDisabled: {
-    color: "green"
-  }
+    color: "green",
+  },
 };
