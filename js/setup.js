f4f749f3ab44a43d72906a9f889b0ac45a1b9609
Simoncar <simoncar@gmail.com>
2019-08-08 15:59:51 +0800

    Global state on loading

--- js/setup.js
+++ js/setup.js
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
 import { StyleProvider, Root } from "native-base";
-
+import { I18nManager, AsyncStorage } from "react-native";
 import App from "./App";
+import I18n from "./lib/i18n";
 import configureStore from "./configureStore";
 import variables from "../native-base-theme/variables/commonColor";
 import getTheme from "../native-base-theme/components";
 import { AppLoading } from "expo";
 import * as Font from "expo-font";
+import _ from "lodash";
 
 export default class Setup extends Component {
   constructor() {
     super();
     this.state = {
-      isLoading: false,
       store: configureStore(() => this.setState({ isLoading: false })),
       isReady: false,
     };
@@ -28,13 +29,47 @@ export default class Setup extends Component {
       //'Material Icons': require('/@expo/vector-icons/fonts/MaterialIcons.ttf'),
     });
 
+    await AsyncStorage.getItem("language").then(language => {
+      if (!_.isString(language)) {
+        language = "en";
+        AsyncStorage.setItem("language", language);
+      }
+      if (language === "ar") {
+        I18nManager.forceRTL(true);
+        if (!I18nManager.isRTL) {
+          Updates.reloadFromCache();
+        }
+      } else {
+        I18nManager.forceRTL(false);
+        if (I18nManager.isRTL) {
+          Updates.reloadFromCache();
+        }
+      }
+      I18n.locale = language;
+      global.language = language;
+    });
+
+    await AsyncStorage.getItem("email").then(email => {
+      global.email = _.isString(email) ? email : "";
+    });
+    await AsyncStorage.getItem("name").then(name => {
+      global.name = _.isString(name) ? name : "";
+      global.authenticated = _.isString(name) ? name : "";
+    });
+    await AsyncStorage.getItem("authenticated").then(authenticated => {
+      global.authenticated = authenticated;
+      console.log(authenticated);
+    });
+
     this.setState({ isReady: true });
   }
 
   render() {
     if (!this.state.isReady) {
+      console.log("....waiting....");
       return <AppLoading />;
     }
+    console.log("....running....");
     return (
       <StyleProvider style={getTheme(variables)}>
         <Root>
