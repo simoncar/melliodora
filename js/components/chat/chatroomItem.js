e69eba9e05de673e7082561007c32e8255134254
Simoncar <simoncar@gmail.com>
2019-08-11 12:04:53 +0800

    Chatroom updates

--- js/components/chat/chatroomItem.js
+++ js/components/chat/chatroomItem.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import { View, TouchableOpacity } from "react-native";
-
 import { Text, Button } from "native-base";
 import { Ionicons, Entypo, Octicons } from "@expo/vector-icons";
+import { Image } from "react-native-expo-image-cache";
 import { Grid, Col, Row } from "react-native-easy-grid";
 import styles from "./styles";
+
+const preview = {
+  uri:
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAHEAAABaCAMAAAC4y0kXAAAAA1BMVEX///+nxBvIAAAAIElEQVRoge3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAPBgKBQAASc1kqgAAAAASUVORK5CYII=",
+};
+const uri =
+  "https://firebasestorage.googleapis.com/v0/b/calendar-app-57e88.appspot.com/o/random%2F201908%2FchatIcon.png?alt=media&token=c5667f8f-efc9-48f6-91a8-5cf57e856505";
 
 class ChatroomItem extends Component {
   constructor(props) {
@@ -28,23 +35,12 @@ class ChatroomItem extends Component {
       >
         <Grid>
           <Col>
-            <View style={{ flexDirection: "row" }}>
-              <Button
-                transparent
-                style={styles.roundedButton}
-                onPress={() => {
-                  this.props.navigation.navigate("chat", {
-                    chatroom: this.props.chatroom,
-                    title: this.props.title,
-                    description: this.props.description,
-                    contact: this.props.contact,
-                    url: this.props.url,
-                    language: this.props.language,
-                  });
-                }}
-              >
-                <Ionicons style={styles.icon} name="ios-chatbubbles" />
-              </Button>
+            <View style={{ paddingLeft: 10, flexDirection: "row" }}>
+              <Image
+                style={{ width: 70, height: 70, borderRadius: 70 / 2 }}
+                {...{ preview, uri }}
+                resizeMode={"contain"}
+              />
 
               <View>
                 <Text style={styles.chatTitle}>{this.props.title}</Text>
