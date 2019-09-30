60d05d6fc9f41cd9d6e030b9a144fd535f748e7f
Simoncar <simoncar@gmail.com>
2019-09-30 12:59:58 +0800

    Moved functions to separate repo

--- functions/computeCounts.js
+++
@@ -1,8 +0,0 @@
-const firebase = require("firebase-admin");
-const moment = require("moment");
-
-//export GOOGLE_APPLICATION_CREDENTIALS="/Users/simon/Documents/code/app/functions/Calendar App-915d5dbe4185.json"
-
-const computeCounts = () => {};
-
-module.exports = computeCounts;
