20640a5c4d6c32f959919948610b2132fb70c0c5
Simoncar <simoncar@gmail.com>
2019-10-15 08:55:40 +0800

    Sentry

--- App.js
+++ App.js
@@ -4,18 +4,24 @@ import Constants from "expo-constants";
 import Sentry from "sentry-expo";
 import _ from "lodash";
 
-Sentry.config("https://66ad14c8bc2c452b943fe68dc6b075ae@sentry.io/185405").install();
+//Sentry.config("https://66ad14c8bc2c452b943fe68dc6b075ae@sentry.io/185405").install();
+
+// Sentry.init({
+//   dsn: "https://66ad14c8bc2c452b943fe68dc6b075ae@sentry.io/185405",
+//   enableInExpoDevelopment: true,
+//   debug: true,
+// });
 
 // Node modules check latest
 // npm-check
 
-export const setTagsContext = (ctx: "env-simulator") => {
-  Sentry.setTagsContext({
-    environment: ctx.environment,
-  });
-};
+// export const setTagsContext = (ctx: "env-simulator") => {
+//   Sentry.setTagsContext({
+//     environment: ctx.environment,
+//   });
+// };
 
-Sentry.captureMessage("App started V" + Constants.manifest.version);
+//Sentry.captureMessage("App started V" + Constants.manifest.version);
 
 export default class App extends Component {
   constructor(props) {
