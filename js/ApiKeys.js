6f39840dd62fcb6ab748c18a79b46b51e657cdbc
Simoncar <simoncar@gmail.com>
2019-11-02 02:25:58 +0800

    Create ApiKeys.js

---
+++ js/ApiKeys.js
@@ -0,0 +1,10 @@
+export default {
+  FirebaseConfig: {
+    apiKey: "XXX",
+    authDomain: "XXX.firebaseapp.com",
+    databaseURL: "https://XXX.firebaseio.com",
+    projectId: "XXX",
+    storageBucket: "XXX.appspot.com",
+    messagingSenderId: "XXX"
+  }
+};
