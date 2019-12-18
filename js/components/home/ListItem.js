c3627ccfcc2b6ff782cf91e7587f81b5128bd2f6
weishenho <weishenbs@gmail.com>
2019-12-18 10:08:57 +0800

    remove grey image when there is no image

--- js/components/home/ListItem.js
+++ js/components/home/ListItem.js
@@ -61,6 +61,7 @@ class ListItem extends Component {
   }
 
   isURL(str) {
+    if (!str) return false;
     var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
       '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
       '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
