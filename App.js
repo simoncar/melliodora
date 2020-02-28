3b969538b2bb3011f43ff4e86d53d27d02321f83
Simoncar <simoncar@gmail.com>
2020-02-29 01:06:03 +0800

    sentry in prod only

--- App.js
+++ App.js
@@ -4,22 +4,16 @@ import * as Sentry from "sentry-expo";
 import Constants from "expo-constants";
 import _ from "lodash";
 
-Sentry.init({
-  dsn: Constants.manifest.extra.sentryDSN,
-  enableInExpoDevelopment: true,
-  debug: true
-});
-
 // Node modules check latest
 // npm-check
 
-export const setTagsContext = (ctx: "env-simulator") => {
-  Sentry.setTagsContext({
-    environment: ctx.environment
-  });
-};
-
 if (Constants.isDevice) {
+  Sentry.init({
+    dsn: Constants.manifest.extra.sentryDSN,
+    enableInExpoDevelopment: true,
+    debug: true
+  });
+
   Sentry.captureMessage("App started V" + Constants.manifest.version);
 }
 export default class App extends Component {
