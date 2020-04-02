d2077f0af1a3e86a65ba5712643604b3cf58dc28
weishen <weishenbs@gmail.com>
2020-04-02 22:34:58 +0800

    title font

--- js/components/home/styles.js
+++ js/components/home/styles.js
@@ -35,7 +35,6 @@ export default {
     color: "black"
   },
   version: {
-    fontFamily: 'SegoeUI',
     backgroundColor: "white",
     color: "#666",
     alignSelf: "center",
@@ -78,7 +77,6 @@ export default {
   },
 
   user: {
-    fontFamily: 'SegoeUI',
     backgroundColor: "white",
     color: "#666",
     alignSelf: "center",
