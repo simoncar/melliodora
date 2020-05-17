38ffbb975ae39c44e7d16627e22a60447e467bd5
Simoncar <simoncar@gmail.com>
2020-05-17 19:10:28 +0800

    firebase analytics

--- js/ApiKeys.js
+++ js/ApiKeys.js
@@ -6,6 +6,8 @@ export default {
     authDomain: "calendar-app-57e88.firebaseapp.com",
     databaseURL: "https://calendar-app-57e88.firebaseio.com",
     projectId: "calendar-app-57e88",
-    storageBucket: "calendar-app-57e88.appspot.com"
-  }
+    storageBucket: "calendar-app-57e88.appspot.com",
+    appId: "1:991350571487:web:64e8030d8ad0c20969c46a",
+    measurementId: "G-0T769CEZVP",
+  },
 };
