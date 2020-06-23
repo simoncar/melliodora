213b6bff64a738e3f7193bb80fa3c5991722e3ad
Simoncar <simoncar@gmail.com>
2020-06-23 09:37:44 +0800

    layout updates

--- src/components/FeatureMoreItems.js
+++ src/components/FeatureMoreItems.js
@@ -1,5 +1,6 @@
+
 import React, { Component } from "react";
-import { View, FlatList } from "react-native";
+import { View, FlatList, StyleSheet } from "react-native";
 import { Image } from "react-native-expo-image-cache";
 
 import firebase from "firebase";
@@ -9,124 +10,105 @@ import { SettingsListItem, Separator } from "./SettingsListItem";
 import { connect } from "react-redux";
 
 class FeatureMoreItems extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      loading: true,
-      featureItems: [],
-    };
-  }
+		this.state = {
+			loading: true,
+			featureItems: []
+		};
+	}
 
-  componentDidMount() {
-    try {
-      this.ref = firebase.firestore().collection(global.domain).doc("feature").collection("features").orderBy("order");
-      this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
-    } catch (e) {
-      console.error(e.message);
-    }
-  }
+	componentDidMount() {
+		try {
+			this.ref = firebase.firestore().collection(global.domain).doc("feature").collection("features").orderBy("order");
+			this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
+		} catch (e) {
+			console.error(e.message);
+		}
+	}
 
-  onCollectionUpdate = (querySnapshot) => {
-    var trans = {};
-    var featureItems = [];
+	onCollectionUpdate = querySnapshot => {
+		var trans = {};
+		var featureItems = [];
 
-    querySnapshot.forEach((doc) => {
-      if (doc.data().translated == true) {
-        trans = {
-          source: "feature",
-          summaryMyLanguage: getLanguageString(this.props.language, doc.data(), "summary"),
-          descriptionMyLanguage: getLanguageString(this.props.language, doc.data(), "description"),
-        };
-      } else {
-        trans = {
-          source: "feature",
-          summaryMyLanguage: doc.data().summary,
-          descriptionMyLanguage: doc.data().description,
-        };
-      }
-      if (this.props.show == "visibleMore") {
-        if (doc.data().visibleMore) {
-          featureItems.push({ ...{ _key: doc.id }, ...doc.data(), ...trans });
-        }
-      } else {
-        featureItems.push({ ...{ _key: doc.id }, ...doc.data(), ...trans });
-      }
-    });
+		querySnapshot.forEach(doc => {
+			if (doc.data().translated == true) {
+				trans = {
+					source: "feature",
+					summaryMyLanguage: getLanguageString(this.props.language, doc.data(), "summary"),
+					descriptionMyLanguage: getLanguageString(this.props.language, doc.data(), "description")
+				};
+			} else {
+				trans = {
+					source: "feature",
+					summaryMyLanguage: doc.data().summary,
+					descriptionMyLanguage: doc.data().description
+				};
+			}
+			if (this.props.show == "visibleMore") {
+				if (doc.data().visibleMore) {
+					featureItems.push({ ...{ _key: doc.id }, ...doc.data(), ...trans });
+				}
+			} else {
+				featureItems.push({ ...{ _key: doc.id }, ...doc.data(), ...trans });
+			}
+		});
 
-    if (featureItems.length > 0) {
-      this.setState({
-        featureItems,
-      });
-    }
+		if (featureItems.length > 0) {
+			this.setState({
+				featureItems
+			});
+		}
 
-    this.setState({
-      loading: false,
-    });
-  };
+		this.setState({
+			loading: false
+		});
+	};
 
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
+	componentWillUnmount() {
+		this.unsubscribe();
+	}
 
-  keyExtractor = (item) => item._key;
+	keyExtractor = item => item._key;
 
-  _renderItem({ item }) {
-    const preview = {
-      uri: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAHEAAABaCAMAAAC4y0kXAAAAA1BMVEX///+nxBvIAAAAIElEQVRoge3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAPBgKBQAASc1kqgAAAAASUVORK5CYII=",
-    };
-    const uri = item.photo1;
+	_renderItem({ item }) {
+		const preview = {
+			uri: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAHEAAABaCAMAAAC4y0kXAAAAA1BMVEX///+nxBvIAAAAIElEQVRoge3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAPBgKBQAASc1kqgAAAAASUVORK5CYII="
+		};
+		const uri = item.photo1;
 
-    if (!(this.props.show == "visibleMore")) {
-      return <ListItem navigation={this.props.navigation} item={item} editMode={this.props.editMode} language={this.props.language} />;
-    } else {
-      return (
-        <SettingsListItem
-          key={"feature2" + item._key}
-          title={item.summaryMyLanguage}
-          icon={
-            <Image
-              style={{
-                marginLeft: 15,
-                alignSelf: "center",
-                width: 30,
-                textAlign: "center",
-
-                height: 30,
-                borderRadius: 18,
-                borderWidth: 0.1,
-                borderColor: "lightgray",
-              }}
-              {...{ preview, uri }}
-            />
-          }
-          onPress={() => this.props.navigation.navigate("storyMore", item)}
-        />
-      );
-    }
-  }
-  _listEmptyComponent = () => {
-    return <View></View>;
-  };
-  render() {
-    if (this.state.loading || !this.state.featureItems > 0) return <View></View>;
-    return (
-      <View>
-        <Separator />
-
-        <FlatList
-          data={this.state.featureItems}
-          keyExtractor={this.keyExtractor}
-          renderItem={this._renderItem.bind(this)}
-          scrollEnabled={false}
-          ListEmptyComponent={this._listEmptyComponent}
-        />
-      </View>
-    );
-  }
+		if (!(this.props.show == "visibleMore")) {
+			return <ListItem navigation={this.props.navigation} item={item} editMode={this.props.editMode} language={this.props.language} />;
+		} else {
+			return <SettingsListItem key={"feature2" + item._key} title={item.summaryMyLanguage} icon={<Image style={styles.a37fe39e0b4f011ea999f193302967c6e} {...{ preview, uri }} />} onPress={() => this.props.navigation.navigate("storyMore", item)} />;
+		}
+	}
+	_listEmptyComponent = () => {
+		return null;
+	};
+	render() {
+		if (this.state.loading || !this.state.featureItems > 0) return <View></View>;
+		return <View>
+			<FlatList data={this.state.featureItems} keyExtractor={this.keyExtractor} renderItem={this._renderItem.bind(this)} scrollEnabled={false} ListEmptyComponent={this._listEmptyComponent} />
+		</View>;
+	}
 }
 
-const mapStateToProps = (state) => ({
-  language: state.auth.language,
+const styles = StyleSheet.create({
+	a37fe39e0b4f011ea999f193302967c6e: {
+		alignSelf: "center",
+		borderColor: "lightgray",
+		borderRadius: 18,
+		borderWidth: 0.1,
+		height: 30,
+		marginLeft: 15,
+		textAlign: "center",
+		width: 30
+	}
+});
+
+const mapStateToProps = state => ({
+	language: state.auth.language
 });
 export default connect(mapStateToProps)(FeatureMoreItems);
