3dba90a2edfc407ef0cee48e9f63da5bf88d1fd8
Simoncar <simoncar@gmail.com>
2020-07-02 14:02:40 +0800

    Update community.js

--- src/store/community.js
+++ src/store/community.js
@@ -64,7 +64,8 @@ function* WORKER_processSelectedCommunity(action) {
 		case "sais_edu_sg":
 			global.switch_portalName = "myStamford";
 			global.switch_tab_portalName = "myS";
-			global.switch_portalURL = "https://mystamford.edu.sg/parent-dashboard";
+			//global.switch_portalURL = "https://mystamford.edu.sg/parent-dashboard";
+			global.switch_portalURL = "https://sais.edu.sg/";
 			global.switch_webportalActions = [
 				{ Home: "https://mystamford.edu.sg/parent-dashboard" },
 				{ "Cafe Top-Up": "https://mystamford.edu.sg/cafe/cafe-online-ordering" },
