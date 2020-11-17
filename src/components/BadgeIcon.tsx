68ced9e92dbd9ca1f7c6e237505959431f558b68
Simoncar <simoncar@gmail.com>
2020-11-17 18:53:19 -0500

    Image selection

---
+++ src/components/BadgeIcon.tsx
@@ -0,0 +1,119 @@
+import * as React from 'react';
+import { Animated, StyleSheet, StyleProp, TextStyle } from 'react-native';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import color from 'color';
+import { black, white } from 'react-native-paper/src/styles/colors';
+import { withTheme } from 'react-native-paper';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const defaultSize = 20;
+
+type Props = React.ComponentProps<typeof Animated.Text> & {
+	/**
+	 * Whether the badge is visible
+	 */
+	visible: boolean;
+	/**
+	 * MaterialCommunityIcons name.
+	 */
+	icon: string;
+	/**
+	 * Size of the `Badge`.
+	 */
+	size?: number;
+	style?: StyleProp<TextStyle>;
+	ref?: React.RefObject<typeof Animated.Text>;
+	/**
+	 * @optional
+	 */
+	theme: ReactNativePaper.Theme;
+};
+
+type State = {
+	opacity: Animated.Value;
+};
+
+class BadgeIcon extends React.Component<Props, State> {
+	static defaultProps = {
+		visible: true,
+		size: defaultSize,
+	};
+
+	state = {
+		opacity: new Animated.Value(this.props.visible ? 1 : 0),
+	};
+
+	componentDidUpdate(prevProps: Props) {
+		const {
+			visible,
+			theme: {
+				animation: { scale },
+			},
+		} = this.props;
+
+		if (visible !== prevProps.visible) {
+			Animated.timing(this.state.opacity, {
+				toValue: visible ? 1 : 0,
+				duration: 150 * scale,
+				useNativeDriver: true,
+			}).start();
+		}
+	}
+
+	render() {
+		const {
+			icon,
+			size = defaultSize,
+			style,
+			theme,
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+			visible,
+			...rest
+		} = this.props;
+		const { opacity } = this.state;
+
+		const { backgroundColor = theme.colors.backdrop, ...restStyle } = StyleSheet.flatten(style) || {};
+		const textColor = color(backgroundColor).isLight() ? black : white;
+
+		const borderRadius = size / 2;
+
+		return (
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			<Animated.Text
+				numberOfLines={1}
+				style={[
+					{
+						opacity,
+						backgroundColor,
+						color: textColor,
+						fontSize: size * 0.5,
+						...theme.fonts.regular,
+						lineHeight: size,
+						height: size,
+						minWidth: size,
+						borderRadius,
+					},
+					styles.container,
+					restStyle,
+				]}
+				{...rest}
+			>
+				<MaterialCommunityIcons name={icon} color={textColor} />
+			</Animated.Text>
+		);
+	}
+}
+
+export default withTheme(BadgeIcon);
+
+const styles = StyleSheet.create({
+	container: {
+		alignSelf: 'flex-end',
+		textAlign: 'center',
+		textAlignVertical: 'center',
+		paddingHorizontal: 4,
+		overflow: 'hidden',
+	},
+});
---
+++ src/screens/ImagePicker.tsx
@@ -0,0 +1,139 @@
+import * as React from 'react';
+import * as MediaLibrary from 'expo-media-library';
+import * as FileSystem from 'expo-file-system';
+import * as Linking from 'expo-linking';
+import { StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import SelectableImageGrid from '../components/SelectableImageGrid';
+import { useCameraRoll } from '../../hooks/useCameraRoll';
+
+
+import { PickerScreenNavProps } from '../navigation/navigation.types';
+
+
+export default function ImagePickerScreen(props: PickerScreenNavProps) {
+	const camRoll = useCameraRoll();
+	const [items, setItems] = React.useState<MediaLibrary.Asset[]>([]);
+
+	const handleSelected = async (selectedItems: MediaLibrary.Asset[]) => {
+
+		const asset = selectedItems
+		console.log("creating: ", asset[0])
+		//const cachedAsset = await MediaLibrary.createAssetAsync(asset[0].uri);
+
+		const promiseFS = FileSystem.copyAsync({ from: asset[0].uri, to: FileSystem.documentDirectory + 'myFile.jpg' })
+		promiseFS.then((ret) => {
+			console.log("promiseFS2: ", ret)
+
+			const promiseInfo = FileSystem.getInfoAsync(FileSystem.documentDirectory + 'myFile.jpg')
+			promiseInfo.then(async (retaa) => {
+				console.log("promiseInfo: ", retaa)
+				const assetXX = await MediaLibrary.createAssetAsync(retaa.uri);
+
+				const promise = MediaLibrary.getAlbumAsync("Imports - Selected")
+
+				promise.then((ret) => {
+					console.log("adding =", retaa.uri)
+
+
+
+					const promise2 = MediaLibrary.addAssetsToAlbumAsync(assetXX, ret.id, true);
+					promise2.then((ret2) => {
+						console.log("ret2=", ret2, asset)
+					})
+						.catch((err) => {
+							return err;
+						});
+				})
+					.catch((err) => {
+						return err;
+					});
+
+				setItems(selectedItems);
+				props.navigation.setOptions({
+					headerTitle: `Speed Pick Shortlist$${selectedItems.length} items selected`,
+				});
+
+			})
+
+		})
+
+
+
+
+	}
+
+
+	const noItems = camRoll.images.length === 0 && !camRoll.isRefreshing;
+
+	if (!camRoll.permissionsGranted) return <NoPermissionsMessage />;
+
+	const onContinue = () => {
+		//props.navigation.push('Prepare', { images: items });
+	};
+
+	return (
+		<View style={styles.container}>
+			{noItems ? (
+				<NoItemsMessage handleRefresh={camRoll.doRefresh} />
+			) : (
+					<SelectableImageGrid
+						images={camRoll.images}
+						onSelectedChange={handleSelected}
+						refreshing={camRoll.isRefreshing}
+						loadingMore={camRoll.isLoadingMore}
+						onRefresh={camRoll.doRefresh}
+						onEndReached={camRoll.loadMore}
+					/>
+				)}
+			<FAB label="Add ..." icon="check-bold" style={styles.fab} visible={items.length > 0} onPress={onContinue} />
+		</View>
+	);
+}
+
+function NoItemsMessage(props: { handleRefresh: () => void }) {
+	return (
+		<View style={styles.messageContainer}>
+			<Text style={styles.messageText}>There are no photos in your camera roll!</Text>
+			<Ionicons name="ios-refresh" onPress={props.handleRefresh} style={styles.refreshIcon} size={20} />
+			<Text style={styles.secondaryText}>Click to refresh</Text>
+		</View>
+	);
+}
+
+function NoPermissionsMessage() {
+	return (
+		<View style={styles.messageContainer}>
+			<Text style={styles.messageText}>You need permissions to access Camera Roll!</Text>
+			<Button onPress={() => Linking.openSettings()}>Open Settings</Button>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	messageContainer: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	messageText: {
+		fontSize: 15,
+	},
+	refreshIcon: {
+		paddingTop: 20,
+		fontSize: 50,
+	},
+	secondaryText: {
+		fontSize: 12,
+		color: '#555',
+	},
+	fab: {
+		position: 'absolute',
+		bottom: 16,
+		left: '30%',
+		right: '30%',
+	},
+});
