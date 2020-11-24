e392e9edf704ea3e2041513b9e08f5a46da979c8
Simoncar <simoncar@gmail.com>
2020-11-24 13:22:40 -0500

    file upload to server

--- src/components/AlbumAPI.ts
+++ src/components/AlbumAPI.ts
@@ -1,4 +1,5 @@
 import * as firebase from "firebase";
+import * as FileSystem from 'expo-file-system';
 
 
 
@@ -35,4 +36,62 @@ export function rebuildAlbum(featureID) {
 					console.log("Done updating album")
 				})
 		})
+
+	storageSend(featureID)
 }
+
+export function storageSend(featureID) {
+
+	console.log("storage send:", featureID)
+	var album = []
+
+	firebase
+		.firestore()
+		.collection(global.domain)
+		.doc("feature")
+		.collection("features")
+		.doc(featureID)
+		.collection("photos")
+		.get()
+		.then(snapshot => {
+			snapshot.forEach(doc => {
+				console.log("doc:", doc.data())
+				album.push(doc.data().local)
+
+				if (doc.data().server === undefined) {
+					console.log("file not on server", doc.data().local)
+					const imageURI = FileSystem.documentDirectory + doc.data().local
+					uploadImage(featureID, imageURI, doc.data().local)
+				}
+			})
+		})
+}
+
+const uploadImage = async (feature: string, imgURI: string, filename: string) => {
+	if (!imgURI) return "";
+
+	var mime = "image/jpeg";
+
+	const blob: any = await new Promise((resolve, reject) => {
+		const xhr = new XMLHttpRequest();
+		xhr.onload = function () {
+			resolve(xhr.response);
+		};
+		xhr.onerror = function (e) {
+			reject(new TypeError("Network request failed"));
+		};
+		xhr.responseType = "blob";
+		xhr.open("GET", imgURI, true);
+		xhr.send(null);
+
+	});
+
+	const ref = firebase.storage().ref("album/2020/11/" + feature + "/" + filename);
+
+	const snapshot = await ref.put(blob, { contentType: mime, cacheControl: 'max-age=31536000' });
+	const downloadURL = await snapshot.ref.getDownloadURL();
+	blob.close();
+
+	console.log("XMLHttpRequest SEND:", imgURI, downloadURL)
+	return downloadURL;
+};
