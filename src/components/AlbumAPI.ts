7811486a0adf6d88681f307f37930348cc946196
Simoncar <simoncar@gmail.com>
2020-11-24 14:00:40 -0500

    server storage

--- src/components/AlbumAPI.ts
+++ src/components/AlbumAPI.ts
@@ -1,12 +1,11 @@
 import * as firebase from "firebase";
 import * as FileSystem from 'expo-file-system';
 
-
-
 export function rebuildAlbum(featureID) {
 
 	console.log("featureID rebuild:", featureID)
-	var album = []
+	var album = {}
+	let i = 0;
 
 	firebase
 		.firestore()
@@ -19,7 +18,12 @@ export function rebuildAlbum(featureID) {
 		.then(snapshot => {
 			snapshot.forEach(doc => {
 				console.log("doc:", doc.data())
-				album.push(doc.data().local)
+				album[i] = {
+					"local": doc.data().local,
+					"server": doc.data().server
+				}
+				i++
+
 			})
 
 			firebase
@@ -62,6 +66,25 @@ export function storageSend(featureID) {
 					console.log("file not on server", doc.data().local)
 					const imageURI = FileSystem.documentDirectory + doc.data().local
 					uploadImage(featureID, imageURI, doc.data().local)
+						.then((downloadURL) => {
+
+							console.log("downloadURL:", downloadURL)
+							firebase
+								.firestore()
+								.collection(global.domain)
+								.doc("feature")
+								.collection("features")
+								.doc(featureID)
+								.collection("photos")
+								.doc(doc.id)
+								.set(
+									{
+										server: downloadURL
+									}, { merge: true })
+								.then(() => {
+									console.log("Done updating album")
+								})
+						})
 				}
 			})
 		})
@@ -91,7 +114,7 @@ const uploadImage = async (feature: string, imgURI: string, filename: string) =>
 	const snapshot = await ref.put(blob, { contentType: mime, cacheControl: 'max-age=31536000' });
 	const downloadURL = await snapshot.ref.getDownloadURL();
 	blob.close();
-
+	console.log("-----")
 	console.log("XMLHttpRequest SEND:", imgURI, downloadURL)
 	return downloadURL;
 };
