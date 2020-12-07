eff2a8cd25f97ef10854852e390d49fdc7671cf6
Simon Cariss <simoncar@gmail.com>
2020-12-06 16:16:10 -0800

    image sizing

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Image, ListRenderItemInfo, View as BareView } from 'react-native';
 import AlbumImage from "./AlbumImage"
 import { listenPhotos } from "./AlbumAPI"
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 interface IProps {
 	feature: string,
@@ -9,17 +10,30 @@ interface IProps {
 	edit: boolean,
 }
 
+function useForceUpdate() {
+	const [value, setValue] = useState(0); // integer state
+	console.log("forceUpdate:", value)
+	return () => setValue(value => ++value); // update the state to force render
+}
+
 export default function ImageList(props: IProps) {
 	const [photos, setPhotos] = useState([]);
 
+	const forceUpdate = useForceUpdate();
+
 	const feature = props.feature
 
-	console.log("feature props:", props)
-
 	useEffect(() => {
+		const subscription = ScreenOrientation.addOrientationChangeListener(() => {
+			console.log('ScreenOrientation change event');
+			forceUpdate()
+		});
 		if (Array.isArray(photos)) {
 			let x = listenPhotos(feature, refreshFunction)
 		}
+		return () => {
+			ScreenOrientation.removeOrientationChangeListeners(subscription);
+		};
 	}, []);
 
 	function refreshFunction(photos) {
