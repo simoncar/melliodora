119202dac6c9b84dfb15047e9d7cc9eded45dcab
Simoncar <simoncar@gmail.com>
2020-12-19 18:26:23 -1000

    fix tests

--- src/components/__tests__/StoryListItem.test.js
+++ src/components/__tests__/StoryListItem.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 
 import ListItem from '../StoryListItem';
-
+jest.mock("../../components/ImageList");
 
 const itemCore = {
 	"source": "feature",
