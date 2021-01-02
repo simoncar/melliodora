0ff289be1b594c890d098ff45bc79ce7e05ea1ea
Simon Cariss <simoncar@gmail.com>
2021-01-02 11:20:54 -0800

    fix logging

--- src/lib/APIUser.ts
+++ src/lib/APIUser.ts
@@ -1,8 +1,8 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import firebase from "../lib/firebase";
 
 import { useAuth } from "../lib/globalState";
-import { AuthUser } from "../lib/interfaces"
+import { AuthUser } from "../lib/interfaces";
 
 interface IAuth {
 	uid: string;
@@ -11,66 +11,56 @@ interface IAuth {
 	email: string;
 }
 
-export function Login(email:string, password:string) {
+export function Login(email: string, password: string) {
+	firebase
+		.auth()
+		.signInWithEmailAndPassword(email, password)
+		.then((auth: firebase.auth.UserCredential) => {
+			const authObj: IAuth = {
+				uid: auth.user.uid,
+				displayName: auth.user.displayName === null ? "" : auth.user.displayName,
+				email: auth.user.email,
+				photoURL: auth.user.photoURL === null ? "" : auth.user.photoURL,
+			};
+		})
+		.catch(function (error) {
+			// Handle Errors here.
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			if (errorCode === "auth/wrong-password") {
+				//setErrorMessage("Wrong password.");
+			} else {
+				//setErrorMessage(errorMessage);
+			}
+			console.log(error);
+		});
+}
 
-		console.log("login:", email, password)
-		firebase
-			.auth()
-			.signInWithEmailAndPassword(email, password)
-			.then((auth: firebase.auth.UserCredential) => {
-
-				const authObj: IAuth = {
-					uid: auth.user.uid,
-					displayName: auth.user.displayName === null ? "" : auth.user.displayName,
-					email: auth.user.email,
-					photoURL: auth.user.photoURL === null ? "" : auth.user.photoURL,
-				};
-
-			})
-			.catch(function (error) {
-				// Handle Errors here.
-				var errorCode = error.code;
-				var errorMessage = error.message;
-				if (errorCode === "auth/wrong-password") {
-					//setErrorMessage("Wrong password.");
-				} else {
-					//setErrorMessage(errorMessage);
-				}
-				console.log(error);
-			});
-};
-	
-export function UpdateUser(user: AuthUser,setGDisplayName:any,setGPhotoURL:any ) {
-
-
-//are you updating yourself?
+export function UpdateUser(user: AuthUser, setGDisplayName: any, setGPhotoURL: any) {
+	//are you updating yourself?
 	var loggedInUser = firebase.auth().currentUser;
 
 	if (loggedInUser != null) {
-
 		if (user.uid === loggedInUser.uid) {
+			loggedInUser
+				.updateProfile({
+					displayName: user.displayName,
+					photoURL: user.photoURL,
+				})
+				.then(function () {
+					// Update successful.
 
-			loggedInUser.updateProfile({
-				displayName: user.displayName,
-				photoURL: user.photoURL,
-			}).then(function () { 
-				// Update successful.
+					firebase.firestore().collection("users").doc(loggedInUser.uid).set(user, { merge: true });
 
-				firebase.firestore()
-					.collection("users")
-					.doc(loggedInUser.uid)
-					.set(user, { merge: true });
-				
-				setGDisplayName(user.displayName);
-				setGPhotoURL(user.photoURL);
-			}).catch(function (error) {
-				// An error happened.
+					setGDisplayName(user.displayName);
+					setGPhotoURL(user.photoURL);
+				})
+				.catch(function (error) {
+					// An error happened.
 					console.log("update failed", error);
-			});
+				});
 		}
-
 	}
 
-	console.log("update:", user.uid, loggedInUser.uid)
-
+	console.log("update:", user.uid, loggedInUser.uid);
 }
