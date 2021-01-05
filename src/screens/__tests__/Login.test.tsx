a04c6de7d4675168b8e599fc7b9286fe853ae8f7
Simon Cariss <simoncar@gmail.com>
2021-01-05 13:10:13 -0800

    fix test

--- src/screens/__tests__/Login.test.tsx
+++ src/screens/__tests__/Login.test.tsx
@@ -1,31 +1,27 @@
-import React from 'react';
-import { render } from '@testing-library/react-native';
-import LoginScreen from '../Login';
+import React from "react";
+import { render } from "@testing-library/react-native";
+import LoginScreen from "../Login";
 
-jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
-jest.mock('../../lib/firebase')
+jest.mock("react-native/Libraries/Animated/src/NativeAnimatedHelper");
+jest.mock("../../lib/firebase");
 
-test('show login screen', () => {
+test("show login screen", () => {
 	const navigation = { navigate: jest.fn() };
 
-	const { toJSON, getByTestId, queryByText } = render(
-		<LoginScreen
-			navigation={navigation} />
-	);
+	const { toJSON, getByTestId, queryByText } = render(<LoginScreen navigation={navigation} />);
 
 	expect(toJSON()).toMatchSnapshot();
-	expect(queryByText("Forgotten Password?")).not.toBeNull();
+	expect(queryByText("Forgot password?")).not.toBeNull();
 	expect(queryByText("Create Account")).not.toBeNull();
 
-	expect(getByTestId('login.email').props).toEqual(
+	expect(getByTestId("login.email").props).toEqual(
 		expect.objectContaining({
-			placeholder: 'Email',
+			placeholder: "Email",
 		})
 	);
-	expect(getByTestId('login.password').props).toEqual(
+	expect(getByTestId("login.password").props).toEqual(
 		expect.objectContaining({
-			placeholder: 'Password',
+			placeholder: "Password",
 		})
 	);
-
 });
